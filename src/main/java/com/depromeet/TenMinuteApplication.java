0604b89899cb7509d3a199608c651375f401c261
kdomo <dongho5088@naver.com>
2023-11-21 00:12:03 +0900

    init: project initial

---
+++ src/main/java/com/depromeet/TenMinuteApplication.java
@@ -0,0 +1,11 @@
+package com.depromeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenMinuteApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TenMinuteApplication.class, args);
+	}
+}
---
+++ src/test/java/com/depromeet/TenMinuteApplicationTests.java
@@ -0,0 +1,11 @@
+package com.depromeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenMinuteApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
