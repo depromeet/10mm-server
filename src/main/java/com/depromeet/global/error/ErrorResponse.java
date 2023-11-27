565518819d35a1d916eaa975dd4ba5b5902077d4
uwoobeat <desktop3711@gmail.com>
2023-11-28 01:42:11 +0900

    feat: 예외 응답 구현

---
+++ src/main/java/com/depromeet/global/error/ErrorResponse.java
@@ -0,0 +1,16 @@
+package com.depromeet.global.error;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+
+public record ErrorResponse(
+	int status,
+	String message,
+	LocalDateTime timestamp
+) {
+
+	public static ErrorResponse of(HttpStatus status, String message) {
+		return new ErrorResponse(status.value(), message, LocalDateTime.now());
+	}
+}
