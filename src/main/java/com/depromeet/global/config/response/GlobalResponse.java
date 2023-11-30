1df613c465fff3a826752c2f493be0c3a3b492b0
kdomo <dongho5088@naver.com>
2023-11-30 16:56:16 +0900

    remove: Getter 제거

--- src/main/java/com/depromeet/global/config/response/GlobalResponse.java
+++ src/main/java/com/depromeet/global/config/response/GlobalResponse.java
@@ -1,9 +1,7 @@
 package com.depromeet.global.config.response;
 
 import java.time.LocalDateTime;
-import lombok.Getter;
 
-@Getter
 public record GlobalResponse(boolean success, int status, Object data, LocalDateTime timestamp) {
     public static GlobalResponse of(int status, Object data) {
         return new GlobalResponse(true, status, data, LocalDateTime.now());
