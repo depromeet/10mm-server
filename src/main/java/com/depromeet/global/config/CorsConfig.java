90224cb9cc58636c9eb5db9d56bfb3034a477419
ybchar <uiurihappy@sju.ac.kr>
2023-12-02 13:49:03 +0900

    chore: cors 기본 설정

---
+++ src/main/java/com/depromeet/global/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.depromeet.global.config;
+
+import java.util.ArrayList;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		ArrayList<String> allowedOriginPatterns = new ArrayList<>();
+		allowedOriginPatterns.add("https://10mm.today");
+		allowedOriginPatterns.add("https://dev.10mm.today");
+
+		String[] patterns = allowedOriginPatterns.toArray(String[]::new);
+		registry.addMapping("/**")
+			.allowedMethods("*")
+			.allowedOriginPatterns(patterns)
+			.allowedHeaders("Authorization", "Content-Type")
+			.exposedHeaders("Set-Cookie")
+			.allowCredentials(true);
+	}
+}
