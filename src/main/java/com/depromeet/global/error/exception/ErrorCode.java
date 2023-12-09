133a5e2531ba89b51a55aa9977d1ff1f8097366a
uwoobeat <desktop3711@gmail.com>
2023-12-09 21:28:10 +0900

    feat: 에러코드 추가

--- src/main/java/com/depromeet/global/error/exception/ErrorCode.java
+++ src/main/java/com/depromeet/global/error/exception/ErrorCode.java
@@ -8,6 +8,12 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
     SAMPLE_ERROR(HttpStatus.BAD_REQUEST, "Sample Error Message"),
+
+    // Member
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 회원을 찾을 수 없습니다."),
+
+    // Security
+    AUTH_NOT_FOUND(HttpStatus.INTERNAL_SERVER_ERROR, "시큐리티 인증 정보를 찾을 수 없습니다."),
     ;
 
     private final HttpStatus status;
