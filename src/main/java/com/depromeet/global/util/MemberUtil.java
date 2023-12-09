8ac8cc7d088c4cd292d6a9e629e429a82c0a6564
uwoobeat <desktop3711@gmail.com>
2023-12-09 21:27:50 +0900

    feat: 멤버 유틸리티 구현

---
+++ src/main/java/com/depromeet/global/util/MemberUtil.java
@@ -0,0 +1,35 @@
+package com.depromeet.global.util;
+
+import com.depromeet.domain.member.dao.MemberRepository;
+import com.depromeet.domain.member.domain.Member;
+import com.depromeet.domain.member.domain.Profile;
+import com.depromeet.global.error.exception.CustomException;
+import com.depromeet.global.error.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class MemberUtil {
+
+	private final SecurityUtil securityUtil;
+	private final MemberRepository memberRepository;
+
+	private void insertMockMemberIfNotExist() {
+		if (memberRepository.count() != 0) {
+			return;
+		}
+
+		Member memeber = Member.createNormalMember(
+			new Profile("testNickname", "testImageUrl"));
+
+		memberRepository.save(memeber);
+	}
+
+	public Member getCurrentMember() {
+		insertMockMemberIfNotExist();
+
+		return memberRepository.findById(securityUtil.getCurrentMemberId())
+				.orElseThrow(() -> new CustomException(ErrorCode.MEMBER_NOT_FOUND));
+	}
+}
