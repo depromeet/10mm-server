1f0b354f89a3904f2afdd861d1148d920c411cef
uwoobeat <desktop3711@gmail.com>
2023-12-09 21:26:30 +0900

    feat: 멤버 레포지터리 추가

--- src/main/java/com/depromeet/domain/member/dao/MemberRepository.java
+++ src/main/java/com/depromeet/domain/member/dao/MemberRepository.java
@@ -3,5 +3,4 @@ package com.depromeet.domain.member.dao;
 import com.depromeet.domain.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
+public interface MemberRepository extends JpaRepository<Member, Long> {}
