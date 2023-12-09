1c93f708b75c0c284b275374f59dfe84e770b8ec
uwoobeat <desktop3711@gmail.com>
2023-12-09 20:23:42 +0900

    refactor: 시큐리티 패키지로 이동

--- src/main/java/com/depromeet/global/config/security/WebSecurityConfig.java
+++ src/main/java/com/depromeet/global/config/security/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.depromeet.global.config;
+package com.depromeet.global.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
