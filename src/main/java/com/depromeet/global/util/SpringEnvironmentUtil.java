c53bb1fd8cfac3fa768afe7a24d61ffbe0e01815
kdomo <dongho5088@naver.com>
2023-12-12 18:55:47 +0900

    style: spotless

--- src/main/java/com/depromeet/global/util/SpringEnvironmentUtil.java
+++ src/main/java/com/depromeet/global/util/SpringEnvironmentUtil.java
@@ -3,11 +3,9 @@ package com.depromeet.global.util;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
-import org.springframework.util.CollectionUtils;
 
 @Component
 @RequiredArgsConstructor
@@ -15,16 +13,16 @@ public class SpringEnvironmentUtil {
     private final Environment environment;
 
     public static final String PROD = "prod";
-	public static String DEV = "dev";
-	public static String LOCAL = "local";
+    public static String DEV = "dev";
+    public static String LOCAL = "local";
 
     private final List<String> PROD_AND_DEV = List.of(PROD, DEV);
 
     public String getCurrentProfile() {
-		return getActiveProfiles()
-			.filter(profile -> profile.equals(PROD) || profile.equals(DEV))
-			.findFirst()
-			.orElse(LOCAL);
+        return getActiveProfiles()
+                .filter(profile -> profile.equals(PROD) || profile.equals(DEV))
+                .findFirst()
+                .orElse(LOCAL);
     }
 
     public Boolean isProdProfile() {
@@ -32,14 +30,14 @@ public class SpringEnvironmentUtil {
     }
 
     public Boolean isDevProfile() {
-		return getActiveProfiles().anyMatch(DEV::equals);
+        return getActiveProfiles().anyMatch(DEV::equals);
     }
 
     public Boolean isProdAndDevProfile() {
-		return getActiveProfiles().anyMatch(PROD_AND_DEV::contains);
+        return getActiveProfiles().anyMatch(PROD_AND_DEV::contains);
     }
 
-	private Stream<String> getActiveProfiles() {
-		return Arrays.stream(environment.getActiveProfiles());
-	}
+    private Stream<String> getActiveProfiles() {
+        return Arrays.stream(environment.getActiveProfiles());
+    }
 }
