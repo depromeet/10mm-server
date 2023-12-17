c2b070e4930b01f70a0000917c187315ed1a7bf5
ybchar <uiurihappy@sju.ac.kr>
2023-12-17 19:45:49 +0900

    fix: spotlessApply

--- src/main/java/com/depromeet/domain/mission/domain/MissionVisibility.java
+++ src/main/java/com/depromeet/domain/mission/domain/MissionVisibility.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum MissionVisibility {
     ALL("전체 공개"),
-	FOLLOWER("팔로워에게 공개"),
+    FOLLOWER("팔로워에게 공개"),
     NONE("비공개");
 
     private final String value;
