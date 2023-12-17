3d9e218f07dc3ae66755806ee2304b8c795efe83
ybchar <uiurihappy@sju.ac.kr>
2023-12-17 19:44:31 +0900

    feat: MissionVisibility 값 추가

--- src/main/java/com/depromeet/domain/mission/domain/MissionVisibility.java
+++ src/main/java/com/depromeet/domain/mission/domain/MissionVisibility.java
@@ -6,8 +6,9 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum MissionVisibility {
-    PUBLIC("공개"),
-    PRIVATE("비공개");
+    ALL("전체 공개"),
+	FOLLOWER("팔로워에게 공개"),
+    NONE("비공개");
 
     private final String value;
 }
