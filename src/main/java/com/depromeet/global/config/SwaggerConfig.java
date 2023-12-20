157be81493dad66fdb63fe9223c19a8bdc1413ca
kdomo <dongho5088@naver.com>
2023-12-21 03:34:26 +0900

    style: spotless

--- src/main/java/com/depromeet/global/config/SwaggerConfig.java
+++ src/main/java/com/depromeet/global/config/SwaggerConfig.java
@@ -13,9 +13,8 @@ import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.security.SecurityScheme.In;
 import io.swagger.v3.oas.models.security.SecurityScheme.Type;
 import io.swagger.v3.oas.models.servers.Server;
-import lombok.RequiredArgsConstructor;
-
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,13 +22,12 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @RequiredArgsConstructor
 public class SwaggerConfig {
-	private static final String SERVER_NAME = "10MM";
-	private static final String API_TITLE = "10MM 서버 API 문서";
-	private static final String API_DESCRIPTION = "10MM 서버 API 문서입니다.";
-	private static final String GITHUB_URL = "https://github.com/depromeet/10mm-server";
+    private static final String SERVER_NAME = "10MM";
+    private static final String API_TITLE = "10MM 서버 API 문서";
+    private static final String API_DESCRIPTION = "10MM 서버 API 문서입니다.";
+    private static final String GITHUB_URL = "https://github.com/depromeet/10mm-server";
 
-
-	private final SpringEnvironmentUtil springEnvironmentUtil;
+    private final SpringEnvironmentUtil springEnvironmentUtil;
 
     @Value("${swagger.version}")
     private String version;
@@ -37,28 +35,26 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-			.servers(swaggerServers())
-			.components(authSetting())
-			.info(swaggerInfo());
+                .servers(swaggerServers())
+                .components(authSetting())
+                .info(swaggerInfo());
     }
 
-	private List<Server> swaggerServers() {
-		Server server = new Server()
-			.url(getServerUrl())
-			.description(API_DESCRIPTION);
-		return List.of(server);
-	}
+    private List<Server> swaggerServers() {
+        Server server = new Server().url(getServerUrl()).description(API_DESCRIPTION);
+        return List.of(server);
+    }
 
-	private String getServerUrl() {
-		switch (springEnvironmentUtil.getCurrentProfile()) {
-			case "prod":
-				return "https://api.10mm.today";
-			case "dev":
-				return "https://dev-api.10mm.today";
-			default:
-				return "https://localhost:8080";
-		}
-	}
+    private String getServerUrl() {
+        switch (springEnvironmentUtil.getCurrentProfile()) {
+            case "prod":
+                return "https://api.10mm.today";
+            case "dev":
+                return "https://dev-api.10mm.today";
+            default:
+                return "https://localhost:8080";
+        }
+    }
 
     private Components authSetting() {
         return new Components()
