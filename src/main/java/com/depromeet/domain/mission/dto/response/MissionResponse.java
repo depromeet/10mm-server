2691d48da1edd4e7eb8857a484f611056b1a033b
ybchar <uiurihappy@sju.ac.kr>
2023-12-26 19:01:18 +0900

    feat: 미션 서비스 테스트 코드 생성, 조회 구현

--- src/main/java/com/depromeet/domain/mission/dto/response/MissionResponse.java
+++ src/main/java/com/depromeet/domain/mission/dto/response/MissionResponse.java
@@ -9,22 +9,22 @@ import lombok.Data;
 public class MissionResponse {
 
     @Schema(description = "미션 ID")
-    private Long missionId;
+    private final Long missionId;
 
     @Schema(description = "미션 이름")
-    private String name;
+    private final String name;
 
     @Schema(description = "미션 내용")
-    private String content;
+    private final String content;
 
     @Schema(description = "미션 카테고리")
-    private MissionCategory category;
+    private final String category;
 
     @Schema(description = "미션 공개여부")
-    private MissionVisibility visibility;
+    private final String visibility;
 
     @Schema(description = "미션 정렬 값")
-    private Integer sort;
+    private final Integer sort;
 
     public MissionResponse(
             Long missionId,
@@ -36,8 +36,8 @@ public class MissionResponse {
         this.missionId = missionId;
         this.name = name;
         this.content = content;
-        this.category = category;
-        this.visibility = visibility;
+        this.category = category.getValue();
+        this.visibility = visibility.getValue();
         this.sort = sort;
     }
 }
--- src/test/java/com/depromeet/domain/mission/service/MissionServiceTest.java
+++ src/test/java/com/depromeet/domain/mission/service/MissionServiceTest.java
@@ -10,10 +10,16 @@ import com.depromeet.domain.mission.domain.Mission;
 import com.depromeet.domain.mission.domain.MissionCategory;
 import com.depromeet.domain.mission.domain.MissionVisibility;
 import com.depromeet.domain.mission.dto.request.CreateMissionRequest;
+import com.depromeet.domain.mission.dto.response.MissionResponse;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Slice;
 
 @SpringBootTest
 class MissionServiceTest {
@@ -22,17 +28,22 @@ class MissionServiceTest {
     @Autowired private MissionRepository missionRepository;
     @Autowired private MemberRepository memberRepository;
 
+    Member member;
+
     @BeforeEach
     void setUp() {
+        memberRepository.deleteAll();
         missionRepository.deleteAll();
+
+        Profile profile = new Profile("testNickname", "testProfileImageUrl");
+        member = Member.createNormalMember(profile);
     }
 
     @Test
-    void 공부_미션_생성() {
+    void 공부_미션_생성_성공() {
         // given
-        Profile profile = new Profile("testNickname", "testProfileImageUrl");
-        Member member = Member.createNormalMember(profile);
         Member saveMember = memberRepository.save(member);
+
         CreateMissionRequest createMissionRequest =
                 new CreateMissionRequest(
                         "testMissionName",
@@ -47,5 +58,107 @@ class MissionServiceTest {
         Mission mission = missionRepository.findAll().get(0);
         assertEquals("testMissionName", mission.getName());
         assertEquals("testMissionContent", mission.getContent());
+    }
+
+    @Test
+    void 미션이름_초과_생성_실패() {
+        // given
+        Member saveMember = memberRepository.save(member);
+
+        CreateMissionRequest createMissionRequest =
+                new CreateMissionRequest(
+                        "testMissionNameMoreThan",
+                        "testMissionContent",
+                        MissionCategory.STUDY,
+                        MissionVisibility.ALL);
+
+        // when
+        DataIntegrityViolationException e =
+                assertThrows(
+                        DataIntegrityViolationException.class,
+                        () -> {
+                            missionService.addMission(createMissionRequest, saveMember.getId());
+                        });
+
+        // expected
+        // 예외 메시지 확인
+        assertTrue(
+                e.getMessage()
+                        .contains("Value too long for column \"NAME CHARACTER VARYING(20)\""));
+    }
+
+    @Test
+    void 미션_단건_조회_성공() {
+        // given
+        Member saveMember = memberRepository.save(member);
+        CreateMissionRequest createMissionRequest =
+                new CreateMissionRequest(
+                        "testMissionName",
+                        "testMissionContent",
+                        MissionCategory.STUDY,
+                        MissionVisibility.ALL);
+        Mission saveMission = missionService.addMission(createMissionRequest, saveMember.getId());
+
+        // when
+        Mission findMission = missionService.findMission(saveMission.getId());
+
+        // expected
+        assertEquals(findMission.getName(), "testMissionName");
+        assertEquals(findMission.getContent(), "testMissionContent");
+        assertEquals(findMission.getCategory().getValue(), "공부");
+        assertEquals(findMission.getVisibility().getValue(), "전체 공개");
+    }
+
+    @Test
+    void 미션_리스트_조회_성공() {
+        // given
+        Member saveMember = memberRepository.save(member);
+        List<CreateMissionRequest> createMissionRequests =
+                IntStream.range(1, 7)
+                        .mapToObj(
+                                i ->
+                                        new CreateMissionRequest(
+                                                "testMissionName_" + i,
+                                                "testMissionContent_" + i,
+                                                MissionCategory.STUDY,
+                                                MissionVisibility.ALL))
+                        .collect(Collectors.toList());
+
+        createMissionRequests.forEach(
+                request -> missionService.addMission(request, saveMember.getId()));
+
+        // when
+        Slice<MissionResponse> missionList = missionService.listMission(saveMember.getId(), 5, 4L);
+
+        // expected
+        assertEquals(missionList.getContent().size(), 5);
+        assertEquals(missionList.getContent().get(0).getName(), "testMissionName_1");
+        assertEquals(missionList.getContent().get(2).getContent(), "testMissionContent_3");
+    }
+
+    @Test
+    void 미션_리스트_조회_마지막_객체_여부() {
+        // given
+        Member saveMember = memberRepository.save(member);
+        List<CreateMissionRequest> createMissionRequests =
+                IntStream.range(1, 7)
+                        .mapToObj(
+                                i ->
+                                        new CreateMissionRequest(
+                                                "testMissionName_" + i,
+                                                "testMissionContent_" + i,
+                                                MissionCategory.STUDY,
+                                                MissionVisibility.ALL))
+                        .collect(Collectors.toList());
+        createMissionRequests.forEach(
+                request -> missionService.addMission(request, saveMember.getId()));
+
+        // when
+        Slice<MissionResponse> missionList =
+                missionService.listMission(saveMember.getId(), 5, null);
+
+        // expected
+        assertEquals(missionList.getContent().size(), 5);
+        assertEquals(missionList.isLast(), false);
     }
 }
