3ee3cc7d8b6690a9b2401608397ba8e0571bd572
ybchar <uiurihappy@sju.ac.kr>
2023-12-31 01:25:36 +0900

    fix: unwrap 이슈로 해결 및 guava 의존성 해제

--- src/test/java/com/depromeet/DatabaseCleaner.java
+++ src/test/java/com/depromeet/DatabaseCleaner.java
@@ -1,15 +1,15 @@
 package com.depromeet;
 
-import com.google.common.base.CaseFormat;
-import jakarta.persistence.Entity;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
-import jakarta.persistence.Table;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 import java.util.stream.Collectors;
+import org.hibernate.Session;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 @Component
 public class DatabaseCleaner implements InitializingBean {
@@ -19,36 +19,34 @@ public class DatabaseCleaner implements InitializingBean {
 
     @Override
     public void afterPropertiesSet() {
+        entityManager.unwrap(Session.class).doWork(this::extractTableNames);
+    }
+
+    private void extractTableNames(Connection conn) {
         tableNames =
                 entityManager.getMetamodel().getEntities().stream()
-                        .filter(e -> e.getJavaType().getAnnotation(Entity.class) != null)
-                        .filter(e -> e.getJavaType().getAnnotation(Table.class) == null)
-                        .map(
-                                e ->
-                                        CaseFormat.UPPER_CAMEL.to(
-                                                CaseFormat.LOWER_UNDERSCORE, e.getName()))
+                        .map(e -> e.getName().replaceAll("([a-z])([A-Z])", "$1_$2").toLowerCase())
                         .collect(Collectors.toList());
     }
 
-    @Transactional
     public void execute() {
-        // 쓰기 지연 저장소에 남은 SQL을 마저 수행
-        entityManager.flush();
-        // 연관 관계 매핑된 테이블 참조 무결성 해제
-        entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY FALSE").executeUpdate();
+        entityManager.unwrap(Session.class).doWork(this::cleanUpDatabase);
+    }
+
+    private void cleanUpDatabase(Connection conn) throws SQLException {
+        Statement statement = conn.createStatement();
+        statement.executeUpdate("SET REFERENTIAL_INTEGRITY FALSE");
 
         for (String tableName : tableNames) {
-            entityManager.createNativeQuery("TRUNCATE TABLE " + tableName).executeUpdate();
-            entityManager
-                    .createNativeQuery(
-                            "ALTER TABLE "
-                                    + tableName
-                                    + " ALTER COLUMN "
-                                    + tableName
-                                    + "_id RESTART WITH 1")
-                    .executeUpdate();
+            statement.executeUpdate("TRUNCATE TABLE " + tableName);
+            statement.executeUpdate(
+                    "ALTER TABLE "
+                            + tableName
+                            + " ALTER COLUMN "
+                            + tableName
+                            + "_id RESTART WITH 1");
         }
 
-        entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY TRUE").executeUpdate();
+        statement.executeUpdate("SET REFERENTIAL_INTEGRITY TRUE");
     }
 }
