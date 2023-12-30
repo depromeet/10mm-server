011090e980ff27d0403a1b121983915b49d0ec09
ybchar <uiurihappy@sju.ac.kr>
2023-12-30 22:51:42 +0900

    fix: MissionController 수정사항 반영

--- src/main/java/com/depromeet/domain/mission/controller/MissionController.java
+++ src/main/java/com/depromeet/domain/mission/controller/MissionController.java
@@ -1,50 +1,65 @@
 package com.depromeet.domain.mission.controller;
 
-import com.depromeet.domain.mission.api.MissionApi;
 import com.depromeet.domain.mission.domain.Mission;
 import com.depromeet.domain.mission.dto.request.CreateMissionRequest;
 import com.depromeet.domain.mission.dto.request.ModifyMissionRequest;
 import com.depromeet.domain.mission.dto.response.MissionResponse;
 import com.depromeet.domain.mission.service.MissionService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Slice;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@Tag(name = "미션 API", description = "미션 관련 API입니다.")
+@RestController
+@RequestMapping("/missions")
 @RequiredArgsConstructor
-public class MissionController implements MissionApi {
+public class MissionController{
 
     private final MissionService missionService;
 
-    @Override
+	@Operation(summary = "미션 생성", description = "미션을 생성합니다.")
+	@PostMapping
     public Mission missionAdd(@Valid CreateMissionRequest createMissionRequest) {
         return missionService.addMission(createMissionRequest);
     }
 
-    @Override
-    public MissionResponse missionDetails(@PathVariable(value = "missionId") Long missionId) {
+	@Operation(summary = "미션 단건 조회", description = "미션을 한 개를 조회합니다.")
+	@GetMapping("/{missionId}")
+    public MissionResponse missionDetails(@PathVariable Long missionId) {
         return missionService.findMission(missionId);
     }
 
-    @Override
+	@Operation(summary = "미션 리스트 조회", description = "미션 리스트를 조회합니다. (무한 스크롤)")
+	@GetMapping
     public Slice<MissionResponse> missionList(
             @RequestParam int size, @RequestParam(required = false) Long lastId) {
         return missionService.listMission(size, lastId);
     }
 
-    @Override
+	@Operation(summary = "미션 단건 수정", description = "단건 미션을 수정합니다.")
+	@PutMapping("/{missionId}")
     public Mission missionModify(
             @Valid @RequestBody ModifyMissionRequest modifyMissionRequest,
-            @PathVariable(value = "missionId") Long missionId) {
+            @PathVariable Long missionId) {
         return missionService.modifyMission(modifyMissionRequest, missionId);
     }
 
-    @Override
-    public void missionRemove(@PathVariable(value = "missionId") Long missionId) {
+	@Operation(summary = "미션 단건 삭제", description = "단건 미션을 삭제합니다.")
+	@DeleteMapping("/{missionId}")
+    public void missionRemove(@PathVariable Long missionId) {
         missionService.removeMission(missionId);
     }
 }
