6cc7f192b9a7cba0f67619aebca0560ecf10e101
ybchar <uiurihappy@sju.ac.kr>
2023-12-31 02:41:43 +0900

    chore: MissionRequest DTO defaultValue 추가

--- src/main/java/com/depromeet/domain/mission/dto/request/MissionUpdateRequest.java
+++ src/main/java/com/depromeet/domain/mission/dto/request/MissionUpdateRequest.java
@@ -8,8 +8,8 @@ import jakarta.validation.constraints.Size;
 public record MissionUpdateRequest(
         @NotBlank(message = "이름은 비워둘 수 없습니다.")
                 @Size(min = 1, max = 20, message = "미션명은 1자 이상 20자 이하")
-                @Schema(description = "미션 이름")
+                @Schema(description = "미션 이름",  defaultValue = "default name")
                 String name,
-        @Size(min = 1, max = 30, message = "미션 내용은 1자 이상 30자 이하") @Schema(description = "미션 내용")
+        @Size(min = 1, max = 30, message = "미션 내용은 1자 이상 30자 이하") @Schema(description = "미션 내용", defaultValue = "default content")
                 String content,
-        @Schema(description = "미션 공개여부") MissionVisibility visibility) {}
+        @Schema(description = "미션 공개여부", defaultValue = "공개") MissionVisibility visibility) {}
