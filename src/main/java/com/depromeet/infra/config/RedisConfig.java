7c0025fe9464b31af5eb90967fc6abe6603eede4
kdomo <dongho5088@naver.com>
2023-12-31 19:31:36 +0900

    chore: redis.yml 생성

--- src/main/java/com/depromeet/infra/config/RedisConfig.java
+++ src/main/java/com/depromeet/infra/config/RedisConfig.java
@@ -13,13 +13,13 @@ import org.springframework.data.redis.repository.configuration.EnableRedisReposi
 @EnableRedisRepositories
 @Configuration
 public class RedisConfig {
-    @Value("${spring.redis.host}")
+    @Value("${spring.data.redis.host}")
     private String redisHost;
 
-    @Value("${spring.redis.port}")
+	@Value("${spring.data.redis.port}")
     private int redisPort;
 
-    @Value("${spring.redis.password}")
+	@Value("${spring.data.redis.password}")
     private String redisPassword;
 
     @Bean
