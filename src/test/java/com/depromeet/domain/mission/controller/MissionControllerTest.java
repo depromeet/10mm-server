bb3ed80e18aaadd927a4e566f79336493918ae6c
ybchar <uiurihappy@sju.ac.kr>
2024-01-04 00:58:01 +0900

    feat: 공부 미션 생성

--- src/test/java/com/depromeet/domain/mission/controller/MissionControllerTest.java
+++ src/test/java/com/depromeet/domain/mission/controller/MissionControllerTest.java
@@ -1,45 +1,33 @@
 package com.depromeet.domain.mission.controller;
 
-import static org.mockito.BDDMockito.*;
-import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.http.MediaType.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import com.depromeet.domain.mission.domain.MissionCategory;
 import com.depromeet.domain.mission.domain.MissionVisibility;
 import com.depromeet.domain.mission.dto.request.MissionCreateRequest;
-import com.depromeet.domain.mission.dto.response.MissionCreateResponse;
-import com.depromeet.domain.mission.service.MissionService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(controllers = MissionControllerTest.class)
+@AutoConfigureMockMvc
+@SpringBootTest
 @ActiveProfiles("test")
-@MockBean(JpaMetamodelMappingContext.class)
 class MissionControllerTest {
 
     @Autowired private MockMvc mockMvc;
-    @MockBean private MissionService missionService;
     @Autowired private ObjectMapper objectMapper;
-    // @Autowired
-    // private DatabaseCleaner databaseCleaner;
-    // @Autowired private MemberUtil memberUtil;
-    //
-    // @BeforeEach
-    // void setUp() {
-    // 	databaseCleaner.execute();
-    // 	System.out.println(memberUtil.getCurrentMember().getId());
-    // }
 
     @Test
+    @WithMockUser(username = "testUser")
     void 공부미션을_생성한다() throws Exception {
         // given
         MissionCreateRequest createRequest =
@@ -48,26 +36,21 @@ class MissionControllerTest {
                         "testMissionContent",
                         MissionCategory.STUDY,
                         MissionVisibility.ALL);
-        given(missionService.createMission(any()))
-                .willReturn(
-                        new MissionCreateResponse(
-                                1L,
-                                createRequest.name(),
-                                createRequest.content(),
-                                createRequest.category(),
-                                createRequest.visibility()));
+
         // expected
         mockMvc.perform(
                         post("/missions")
+                                .accept(APPLICATION_JSON)
                                 .contentType(APPLICATION_JSON)
+                                .with(csrf())
                                 .content(objectMapper.writeValueAsString(createRequest)))
                 .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.missionId").exists())
-                .andExpect(jsonPath("$.name").exists())
-                .andExpect(jsonPath("$.name").value("testMissionName"))
-                .andExpect(jsonPath("$.content").value("testMissionContent"))
-                .andExpect(jsonPath("$.category").value("STUDY"))
-                .andExpect(jsonPath("$.visibility").value("ALL"))
+                .andExpect(jsonPath("$.data.missionId").exists())
+                .andExpect(jsonPath("$.data.name").exists())
+                .andExpect(jsonPath("$.data.name").value("testMissionName"))
+                .andExpect(jsonPath("$.data.content").value("testMissionContent"))
+                .andExpect(jsonPath("$.data.category").value("STUDY"))
+                .andExpect(jsonPath("$.data.visibility").value("ALL"))
                 .andDo(print());
     }
 }
