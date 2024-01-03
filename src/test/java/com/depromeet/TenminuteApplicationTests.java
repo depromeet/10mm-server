ff48812f164c5a582a684d865ed559ad9fddd964
도모 <dongho5088@naver.com>
2024-01-03 18:59:06 +0900

    chore: Redis 설정 (#88)
    * chore: starter-data-redis 의존성 추가
    
    * chore: redis config 추가
    
    * chore: redis.yml 생성
    
    * fix: redis yml deprecated 되어 config Value값 변경
    
    * test: ActiveProfiles test 값 추가
    
    * test: test yml에 redis 정보 추가
    
    * chore: test를 위한 docker-compose-test.yml 생성
    
    * chore: github actions 빌드 시 test redis 사용
    
    * remove: .gitkpeep 삭제
    
    * fix: build시 docker-compose-test.yml 사용하도록 변경

--- src/test/java/com/depromeet/TenminuteApplicationTests.java
+++ src/test/java/com/depromeet/TenminuteApplicationTests.java
@@ -2,8 +2,10 @@ package com.depromeet;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TenminuteApplicationTests {
     @Test
     void contextLoads() {}
--- src/test/java/com/depromeet/global/util/MemberUtilTest.java
+++ src/test/java/com/depromeet/global/util/MemberUtilTest.java
@@ -10,8 +10,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MemberUtilTest {
 
     @Autowired private MemberUtil memberUtil;
