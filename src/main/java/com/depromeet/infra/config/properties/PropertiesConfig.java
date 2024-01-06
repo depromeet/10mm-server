42c94d0076c2e1c17aceb60e5c122c13a114d541
uwoobeat <desktop3711@gmail.com>
2024-01-07 08:42:39 +0900

    feat: JwtProperties 추가

--- src/main/java/com/depromeet/infra/config/properties/PropertiesConfig.java
+++ src/main/java/com/depromeet/infra/config/properties/PropertiesConfig.java
@@ -1,10 +1,15 @@
 package com.depromeet.infra.config.properties;
 
+import com.depromeet.infra.config.jwt.JwtProperties;
 import com.depromeet.infra.config.redis.RedisProperties;
 import com.depromeet.infra.config.storage.StorageProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-@EnableConfigurationProperties({StorageProperties.class, RedisProperties.class})
+@EnableConfigurationProperties({
+    StorageProperties.class,
+    RedisProperties.class,
+    JwtProperties.class
+})
 @Configuration
 public class PropertiesConfig {}
