959f7e79b2ddec9ef2841deca8c00d84047c1b74
ybchar <uiurihappy@sju.ac.kr>
2024-01-08 15:48:17 +0900

    fix: ReactionType 기획서 반영

--- src/main/java/com/depromeet/domain/reaction/domain/ReactionType.java
+++ src/main/java/com/depromeet/domain/reaction/domain/ReactionType.java
@@ -6,20 +6,13 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ReactionType {
-    TENTEN("십분이"),
-    LIKE("좋아요"),
-    LOVE("하트"),
-    THUMB_SUP("최고"),
-    STAR("별"),
-    UNICORN("유니콘"),
-    SAD("슬퍼요"),
-    CHILLING("휴식"),
-    BOOK("책"),
-    FOLDED_HAND("모은 손"),
-    LAPTOP("노트북"),
-    MUSIC("음악"),
-    MUSCLE("근육"),
-    GUN("총");
+    PURPLE_HEART("PURPLE_HEART"),
+    STAR("STAR"),
+    RED_HEART("RED_HEART"),
+    FIRECRACKER("FIRECRACKER"),
+    UNICORN("UNICORN"),
+    FIRE("FIRE"),
+    EYE("EYE");
 
     private final String value;
 }
