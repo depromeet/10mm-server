aadb6d91ac8f877ff25cea8f9c7f0129532f5d73
uwoobeat <desktop3711@gmail.com>
2024-01-10 03:12:50 +0900

    feat: 시큐리티 관련 상수 클래스 추가

---
+++ src/main/java/com/depromeet/global/common/constants/SecurityConstants.java
@@ -0,0 +1,8 @@
+package com.depromeet.global.common.constants;
+
+public final class SecurityConstants {
+    public static final String TOKEN_ROLE_NAME = "role";
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String ACCESS_TOKEN_HEADER = "Authorization";
+    public static final String REFRESH_TOKEN_HEADER = "Refresh-Token";
+}
