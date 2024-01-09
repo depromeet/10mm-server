71ca490fb91b60e5dc551b9f00cd4500a74b62da
uwoobeat <desktop3711@gmail.com>
2024-01-10 04:55:07 +0900

    test: 리프레시 토큰 레포지터리 테스트 작성

---
+++ src/test/java/com/depromeet/domain/token/dao/RefreshTokenRepositoryTest.java
@@ -0,0 +1,70 @@
+package com.depromeet.domain.token.dao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.depromeet.TestRedisConfig;
+import com.depromeet.domain.token.domain.RefreshToken;
+import java.util.Optional;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.redis.DataRedisTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@Import(TestRedisConfig.class)
+@DataRedisTest
+class RefreshTokenRepositoryTest {
+
+    @Autowired private RefreshTokenRepository refreshTokenRepository;
+
+    @AfterEach
+    public void tearDown() {
+        refreshTokenRepository.deleteAll();
+    }
+
+    @Test
+    void 리프레시_토큰을_저장한다() {
+        // given
+        RefreshToken refreshToken =
+                RefreshToken.builder().memberId(1L).token("testRefreshToken").ttl(1000).build();
+
+        // when
+        refreshTokenRepository.save(refreshToken);
+
+        // then
+        Optional<RefreshToken> savedRefreshToken = refreshTokenRepository.findById(1L);
+        assertTrue(savedRefreshToken.isPresent());
+    }
+
+    @Test
+    void 리프레시_토큰을_삭제한다() {
+        // given
+        RefreshToken refreshToken =
+                RefreshToken.builder().memberId(2L).token("testRefreshToken").ttl(1000).build();
+        refreshTokenRepository.save(refreshToken);
+
+        // when
+        refreshTokenRepository.delete(refreshToken);
+
+        // then
+        assertFalse(refreshTokenRepository.findById(refreshToken.getMemberId()).isPresent());
+    }
+
+    @Test
+    void 리프레시_토큰을_조회한다() {
+        // given
+        RefreshToken refreshToken =
+                RefreshToken.builder().memberId(3L).token("testRefreshToken").ttl(1000).build();
+        refreshTokenRepository.save(refreshToken);
+
+        // when
+        Optional<RefreshToken> savedRefreshToken = refreshTokenRepository.findById(3L);
+
+        // then
+        assertTrue(savedRefreshToken.isPresent());
+        assertEquals(3L, savedRefreshToken.get().getMemberId());
+        assertEquals("testRefreshToken", savedRefreshToken.get().getToken());
+    }
+}
