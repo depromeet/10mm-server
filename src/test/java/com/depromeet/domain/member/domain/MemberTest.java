a16eed1a6bc0f31bd78f8192e1ce8da7b17591cc
uwoobeat <desktop3711@gmail.com>
2024-01-10 05:37:55 +0900

    test: 멤버 도메인 유닛 테스트 변경 및 회원가입 테스트 작성

--- src/test/java/com/depromeet/domain/member/domain/MemberTest.java
+++ src/test/java/com/depromeet/domain/member/domain/MemberTest.java
@@ -1,9 +1,15 @@
 package com.depromeet.domain.member.domain;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.depromeet.global.error.exception.CustomException;
+import com.depromeet.global.error.exception.ErrorCode;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
 
 class MemberTest {
 
@@ -18,7 +24,7 @@ class MemberTest {
     @Test
     void 회원가입시_초기_상태는_NORMAL이다() {
         // given
-        Member member = Member.createNormalMember(profile);
+        Member member = Member.createGuestMember(new OauthInfo("testProvider", "testProviderId"));
 
         // when
         MemberStatus status = member.getStatus();
@@ -28,26 +34,74 @@ class MemberTest {
     }
 
     @Test
-    void 회원가입시_초기_역할은_USER이다() {
+    void 회원가입시_초기_역할은_GUEST이다() {
         // given
-        Member member = Member.createNormalMember(profile);
+        Member member = Member.createGuestMember(new OauthInfo("testProvider", "testProviderId"));
 
         // when
         MemberRole role = member.getRole();
 
         // then
-        assertEquals(MemberRole.USER, role);
+        assertEquals(MemberRole.GUEST, role);
     }
 
     @Test
     void 회원가입시_초기_공개여부는_PUBLIC이다() {
         // given
-        Member member = Member.createNormalMember(profile);
+        Member member = Member.createGuestMember(new OauthInfo("testProvider", "testProviderId"));
 
         // when
         MemberVisibility visibility = member.getVisibility();
 
         // then
         assertEquals(MemberVisibility.PUBLIC, visibility);
+    }
+
+    @Test
+    void 마지막_로그인_시간을_업데이트한다() {
+        // given
+        Member member = Member.createNormalMember(profile);
+        LocalDateTime lastLoginAt = LocalDateTime.of(2024, 1, 10, 0, 0);
+
+        // when
+        member.updateLastLoginAt(lastLoginAt);
+
+        // then
+        assertEquals(lastLoginAt, member.getLastLoginAt());
+    }
+
+    @Test
+    void 회원가입시_게스트멤버의_닉네임이_설정된다() {
+        // given
+        Member member = Member.createGuestMember(new OauthInfo("testProvider", "testProviderId"));
+
+        // when
+        member.register("testNickname");
+
+        // then
+        assertEquals("testNickname", member.getProfile().getNickname());
+    }
+
+    @Test
+    void 회원가입시_게스트멤버는_일반멤버로_변경된다() {
+        // given
+        Member member = Member.createGuestMember(new OauthInfo("testProvider", "testProviderId"));
+
+        // when
+        member.register("testNickname");
+
+        // then
+        assertEquals(MemberRole.USER, member.getRole());
+    }
+
+    @Test
+    void 회원가입시_일반멤버이면_예외가_발생한다() {
+        // given
+        Member member = Member.createNormalMember(profile);
+
+        // when & then
+        assertThatThrownBy(() -> member.register("testNickname"))
+                .isInstanceOf(CustomException.class)
+                .hasMessage(ErrorCode.MEMBER_ALREADY_REGISTERED.getMessage());
     }
 }
