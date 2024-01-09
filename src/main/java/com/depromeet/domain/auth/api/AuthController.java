7b9428c47affb414820d255485623c24b2075e6d
uwoobeat <desktop3711@gmail.com>
2024-01-10 08:17:09 +0900

    feat: 회원가입 서비스 및 컨트롤러 구현

---
+++ src/main/java/com/depromeet/domain/auth/api/AuthController.java
@@ -0,0 +1,31 @@
+package com.depromeet.domain.auth.api;
+
+import com.depromeet.domain.auth.application.AuthService;
+import com.depromeet.domain.auth.application.JwtTokenService;
+import com.depromeet.domain.auth.dto.MemberRegisterRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "[인증]", description = "인증 관련 API")
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final JwtTokenService jwtTokenService;
+    private final AuthService authService;
+
+    @Operation(summary = "회원가입", description = "회원가입을 진행합니다.")
+    @PostMapping("/register")
+    public ResponseEntity<Void> memberRegister(@Valid @RequestBody MemberRegisterRequest request) {
+        authService.registerMember(request);
+        return ResponseEntity.ok().build();
+    }
+}
