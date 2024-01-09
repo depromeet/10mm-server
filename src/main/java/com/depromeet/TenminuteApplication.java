1262542c1d882ddb750570bc1c3cc739c2de3469
uwoobeat <desktop3711@gmail.com>
2024-01-10 04:30:57 +0900

    refactor: 테스트 시 JPA 관련 빈 로딩 제외를 위해 설정 클래스 분리

--- src/main/java/com/depromeet/TenminuteApplication.java
+++ src/main/java/com/depromeet/TenminuteApplication.java
@@ -2,10 +2,8 @@ package com.depromeet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class TenminuteApplication {
     public static void main(String[] args) {
         SpringApplication.run(TenminuteApplication.class, args);
