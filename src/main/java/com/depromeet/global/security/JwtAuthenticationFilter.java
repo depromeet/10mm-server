ce2f65efa98b326a2bb9ee91aa1ed2bab2853b9c
uwoobeat <desktop3711@gmail.com>
2024-01-10 08:20:40 +0900

    feat: JWT 인증 필터 구현

---
+++ src/main/java/com/depromeet/global/security/JwtAuthenticationFilter.java
@@ -0,0 +1,56 @@
+package com.depromeet.global.security;
+
+import static com.depromeet.global.common.constants.SecurityConstants.*;
+
+import com.depromeet.domain.auth.application.JwtTokenService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Slf4j
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtTokenService jwtTokenService;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String accessToken = extractAccessToken(request);
+        String refreshToken = extractRefreshToken(request);
+
+        if (accessToken == null || refreshToken == null) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        Authentication authentication = jwtTokenService.getAuthentication(accessToken);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String extractRefreshToken(HttpServletRequest request) {
+        return Optional.ofNullable(request.getHeader(REFRESH_TOKEN_HEADER))
+                .filter(token -> token.startsWith(TOKEN_PREFIX))
+                .map(token -> token.replace(TOKEN_PREFIX, ""))
+                .orElse(null);
+    }
+
+    private String extractAccessToken(HttpServletRequest request) {
+        return Optional.ofNullable(request.getHeader(ACCESS_TOKEN_HEADER))
+                .filter(token -> token.startsWith(TOKEN_PREFIX))
+                .map(token -> token.replace(TOKEN_PREFIX, ""))
+                .orElse(null);
+    }
+}
