a4d525c74c068b6b4bf19160f5f15fdd43f6c176
uwoobeat <desktop3711@gmail.com>
2024-01-11 19:35:20 +0900

    refactor: 커스텀 헤더 이름 변경

--- src/main/java/com/depromeet/global/common/constants/SecurityConstants.java
+++ src/main/java/com/depromeet/global/common/constants/SecurityConstants.java
@@ -5,5 +5,5 @@ public final class SecurityConstants {
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String ACCESS_TOKEN_HEADER = "Authorization";
     public static final String REFRESH_TOKEN_HEADER = "Refresh-Token";
-    public static final String REGISTER_REQUIRED_HEADER = "Register-Required";
+    public static final String REGISTER_REQUIRED_HEADER = "Registration-Required";
 }
