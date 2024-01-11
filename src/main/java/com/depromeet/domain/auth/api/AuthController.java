240a02017e5f87c5e961de935ca8ea0db514a9f1
uwoobeat <desktop3711@gmail.com>
2024-01-11 20:44:05 +0900

    docs: 인증 API 스웨거 태그 추가

--- src/main/java/com/depromeet/domain/auth/api/AuthController.java
+++ src/main/java/com/depromeet/domain/auth/api/AuthController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "[인증]", description = "인증 관련 API")
+@Tag(name = "1. [인증]", description = "인증 관련 API")
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
