bd00cfdfdc5d99e40955617ac506566b264fabd6
uwoobeat <desktop3711@gmail.com>
2024-01-15 05:09:00 +0900

    refactor: 패키지 이동

--- src/main/java/com/depromeet/global/security/PrincipalDetails.java
+++ src/main/java/com/depromeet/global/security/PrincipalDetails.java
@@ -1,11 +1,13 @@
-package com.depromeet.global.config.security;
+package com.depromeet.global.security;
 
-import java.util.Collection;
-import java.util.Collections;
 import lombok.AllArgsConstructor;
+
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
 
 @AllArgsConstructor
 public class PrincipalDetails implements UserDetails {
