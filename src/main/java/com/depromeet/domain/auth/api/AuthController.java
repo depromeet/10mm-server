d79e01a3f2b7753c38b63163baf8fd873b4020c8
uwoobeat <desktop3711@gmail.com>
2024-01-15 05:30:17 +0900

    feat: 임시 회원가입 및 로그인 컨트롤러

--- src/main/java/com/depromeet/domain/auth/api/AuthController.java
+++ src/main/java/com/depromeet/domain/auth/api/AuthController.java
@@ -2,11 +2,16 @@ package com.depromeet.domain.auth.api;
 
 import com.depromeet.domain.auth.application.AuthService;
 import com.depromeet.domain.auth.application.JwtTokenService;
+import com.depromeet.domain.auth.dto.UsernamePasswordRequest;
 import com.depromeet.domain.auth.dto.MemberRegisterRequest;
+
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+
 import jakarta.validation.Valid;
+
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,6 +31,20 @@ public class AuthController {
     @PostMapping("/register")
     public ResponseEntity<Void> memberRegister(@Valid @RequestBody MemberRegisterRequest request) {
         authService.registerMember(request);
+        return ResponseEntity.ok().build();
+    }
+
+    @Operation(summary = "아이디/비밀번호 임시 회원가입", description = "아이디/비밀번호 임시 회원가입을 진행합니다.")
+    @PostMapping("/register/temp")
+    public ResponseEntity<Void> memberTempRegister(
+            @Valid @RequestBody MemberRegisterRequest request) {
+        authService.registerWithUsernameAndPassword(request);
+        return ResponseEntity.ok().build();
+    }
+
+    @Operation(summary = "로그인", description = "로그인을 진행합니다.")
+    @PostMapping("/login")
+    public ResponseEntity<Void> memberLogin(@Valid @RequestBody UsernamePasswordRequest request) {
         return ResponseEntity.ok().build();
     }
 }
---
+++ src/main/java/com/depromeet/domain/auth/dto/UsernamePasswordRequest.java
@@ -0,0 +1,13 @@
+package com.depromeet.domain.auth.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import jakarta.validation.constraints.NotNull;
+
+public record UsernamePasswordRequest(
+        @NotNull(message = "아이디는 비워둘 수 없습니다.")
+                @Schema(description = "회원 아이디", defaultValue = "username")
+                String username,
+        @NotNull(message = "비밀번호는 비워둘 수 없습니다.")
+                @Schema(description = "회원 비밀번호", defaultValue = "password")
+                String password) {}
