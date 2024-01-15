cc5a1f6315dbe531a7d9b169e82647b24f77e0cc
uwoobeat <desktop3711@gmail.com>
2024-01-15 20:15:26 +0900

    fix: 픽스처 삽입 로직 제거

--- src/main/java/com/depromeet/global/util/MemberUtil.java
+++ src/main/java/com/depromeet/global/util/MemberUtil.java
@@ -2,7 +2,6 @@ package com.depromeet.global.util;
 
 import com.depromeet.domain.member.dao.MemberRepository;
 import com.depromeet.domain.member.domain.Member;
-import com.depromeet.domain.member.domain.Profile;
 import com.depromeet.global.error.exception.CustomException;
 import com.depromeet.global.error.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
@@ -15,20 +14,7 @@ public class MemberUtil {
     private final SecurityUtil securityUtil;
     private final MemberRepository memberRepository;
 
-    // TODO: 데이터베이스 연동 및 픽스쳐 데이터 삽입 이후 삭제
-    private void insertMockMemberIfNotExist() {
-        if (memberRepository.count() != 0) {
-            return;
-        }
-
-        Member memeber = Member.createNormalMember(new Profile("testNickname", "testImageUrl"));
-
-        memberRepository.save(memeber);
-    }
-
     public Member getCurrentMember() {
-        insertMockMemberIfNotExist();
-
         return memberRepository
                 .findById(securityUtil.getCurrentMemberId())
                 .orElseThrow(() -> new CustomException(ErrorCode.MEMBER_NOT_FOUND));
