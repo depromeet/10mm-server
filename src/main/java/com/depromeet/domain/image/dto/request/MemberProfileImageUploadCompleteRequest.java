bde2d86b47de82bb313887bf44cd8d7af0e9c021
도모 <dongho5088@naver.com>
2024-01-16 21:36:16 +0900

    fix: 회원 정보 수정할 때 이미지 nullable 처리 (#166)
    * fix: imageFileExtension nullable 처리
    
    * fix: 닉네임 Notnull 처리
    
    * fix: 이미지 null로 넘어왔을 경우 닉네임만 수정
    
    * style: spotless

--- src/main/java/com/depromeet/domain/image/dto/request/MemberProfileImageUploadCompleteRequest.java
+++ src/main/java/com/depromeet/domain/image/dto/request/MemberProfileImageUploadCompleteRequest.java
@@ -5,7 +5,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 
 public record MemberProfileImageUploadCompleteRequest(
-        @NotNull(message = "이미지 파일의 확장자는 비워둘 수 없습니다.")
-                @Schema(description = "이미지 파일의 확장자", defaultValue = "JPEG")
+        @Schema(description = "이미지 파일의 확장자", defaultValue = "JPEG")
                 ImageFileExtension imageFileExtension,
-        @Schema(description = "닉네임", defaultValue = "당근조이") String nickname) {}
+        @NotNull(message = "닉네임은 비워둘 수 없습니다.") @Schema(description = "닉네임", defaultValue = "당근조이")
+                String nickname) {}
