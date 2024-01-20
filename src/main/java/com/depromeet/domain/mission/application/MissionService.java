7edd9f84d06f12a0a6acdd3f26029305cf33cf03
ybchar <uiurihappy@sju.ac.kr>
2024-01-20 20:28:37 +0900

    fix: 달성률 계산 메서드 분리

--- src/main/java/com/depromeet/domain/mission/application/MissionService.java
+++ src/main/java/com/depromeet/domain/mission/application/MissionService.java
@@ -136,17 +136,21 @@ public class MissionService {
          * 분 : (초 % 3600) / 60
          */
         long totalMissionHour = sumDuration / 3600;
-        long totalMissionMinute = (sumDuration % 3600) / 60;
+        long totalMissionMinute = totalMissionHour / 60;
 
-        /* 달성률
-        계산식: (완료된 미션 수 / 전체 미션 수 * 1000.0) / 10.0
-        소수점 첫 째 자리까지
-         */
         double totalMissionAttainRate =
-                Math.round((double) completeMissionSize / totalMissionRecordSize * 1000) / 10.0;
+                calculateMissionAttainRate(completeMissionSize, totalMissionRecordSize);
 
         return MissionRecordSummaryResponse.from(
                 symbolStack, totalMissionHour, totalMissionMinute, totalMissionAttainRate);
+    }
+
+    /* 달성률
+    계산식: (완료된 미션 수 / 전체 미션 수 * 1000.0) / 10.0
+    소수점 첫 째 자리까지
+     */
+    private double calculateMissionAttainRate(long completeSize, long totalSize) {
+        return Math.round((double) completeSize / totalSize * 1000) / 10.0;
     }
 
     public MissionUpdateResponse updateMission(
