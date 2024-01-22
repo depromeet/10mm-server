d9e1c8adbd19ac0712bef05fe0cc380d8b6b0dd6
kdomo <dongho5088@naver.com>
2024-01-22 22:09:30 +0900

    fix: enum 응답 값 JsonFormat 삭제

--- src/main/java/com/depromeet/domain/follow/dto/response/FollowStatus.java
+++ src/main/java/com/depromeet/domain/follow/dto/response/FollowStatus.java
@@ -1,12 +1,10 @@
 package com.depromeet.domain.follow.dto.response;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-@JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum FollowStatus {
     FOLLOWING("팔로잉"), // A가 이미 B를 팔로우 중일때 (팔로우 취소)
     FOLLOWED_BY_ME("맞팔로우"), // B가 A를 팔로우 중일때 (맞팔로우)
