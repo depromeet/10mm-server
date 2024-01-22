508b2d6fb0eff71b87f565440d21fb891ba38c85
kdomo <dongho5088@naver.com>
2024-01-22 20:25:51 +0900

    refactor: FollowStatus value 변경

--- src/main/java/com/depromeet/domain/follow/dto/response/FollowStatus.java
+++ src/main/java/com/depromeet/domain/follow/dto/response/FollowStatus.java
@@ -9,7 +9,7 @@ import lombok.Getter;
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum FollowStatus {
     FOLLOWING("팔로잉"), // A가 이미 B를 팔로우 중일때 (팔로우 취소)
-    FOLLOWED_BY_ME("맞팔로우 하기"), // B가 A를 팔로우 중일때 (맞팔로우 하기)
+    FOLLOWED_BY_ME("맞팔로우"), // B가 A를 팔로우 중일때 (맞팔로우)
     NOT_FOLLOWING("팔로우"), // A가 B를 팔로우하지 않고, B도 A를 팔로우하지 않을 때 (팔로우 추가)
     ;
 
