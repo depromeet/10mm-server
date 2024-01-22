78c3f67ec00268ab1a4d301ada5033c994257ad5
Jaehyun Ahn <91878695+uwoobeat@users.noreply.github.com>
2024-01-23 02:26:59 +0900

    feat: 소셜 로그인 시 SDK 방식을 사용하도록 변경 (#195)
    * refactor: 스프링 시큐리티 OAuth 관련 클래스 제거
    
    * feat: id token 검증 클래스 구현
    
    * feat: 소셜 로그인 기능 구현
    
    * style: spotless 적용
    
    * fix: post 방식으로 변경
    
    * style: spotless 적용
    
    * refactor: 미사용 의존성 제거
    
    * fix: 디폴트 논스값 넣어주도록 변경
    
    * feat: issuer 상수 추가
    
    * refactor: OidcProperties 사용하도록 개선
    
    * refactor: PropertyBinder 사용하도록 개선
    
    * fix: 오타 수정
    
    * fix: 초기화 순서 변경
    
    * chore: 테스트 트리거 활성화
    
    * fix: 검증 시 여러 개의 aud 값을 지원하도록 수정
    
    * refactor: 요청 파라미터에 이름 지정
    
    * fix: nonce 로직 수정 및 투두 추가
    
    * style: spotless 적용
    
    * chore: 테스트 트리거 롤백
    
    * docs: 투두 추가

---
+++ src/main/java/com/depromeet/domain/auth/application/IdTokenVerifier.java
@@ -0,0 +1,105 @@
+package com.depromeet.domain.auth.application;
+
+import com.depromeet.domain.auth.domain.OauthProvider;
+import com.depromeet.global.error.exception.CustomException;
+import com.depromeet.global.error.exception.ErrorCode;
+import com.depromeet.infra.config.oidc.OidcProperties;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import java.util.List;
+import java.util.Map;
+import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class IdTokenVerifier {
+
+    private final OidcProperties oidcProperties;
+    private final Map<OauthProvider, PropertyBinder> properties;
+
+    public IdTokenVerifier(OidcProperties oidcProperties) {
+        this.oidcProperties = oidcProperties;
+        this.properties =
+                Map.of(
+                        OauthProvider.KAKAO,
+                        new PropertyBinder(
+                                buildDecoder(oidcProperties.kakao().jwkSetUri()),
+                                oidcProperties.kakao().issuer(),
+                                oidcProperties.kakao().audience()),
+                        OauthProvider.APPLE,
+                        new PropertyBinder(
+                                buildDecoder(oidcProperties.apple().jwkSetUri()),
+                                oidcProperties.apple().issuer(),
+                                oidcProperties.apple().audience()));
+    }
+
+    private JwtDecoder buildDecoder(String jwkUrl) {
+        return NimbusJwtDecoder.withJwkSetUri(jwkUrl).build();
+    }
+
+    public OidcUser getOidcUser(String idToken, OauthProvider provider) {
+        Jwt jwt = getJwt(idToken, provider);
+        OidcIdToken oidcIdToken = getOidcIdToken(jwt);
+        validateIssuer(oidcIdToken, provider);
+        validateAudience(oidcIdToken, provider);
+        validateNonce(oidcIdToken);
+        return new DefaultOidcUser(null, oidcIdToken);
+    }
+
+    private void validateAudience(OidcIdToken oidcIdToken, OauthProvider provider) {
+        String idTokenAudience = oidcIdToken.getAudience().get(0);
+        List<String> targetAudiences = properties.get(provider).audience();
+
+        if (idTokenAudience == null || !targetAudiences.contains(idTokenAudience)) {
+            throw new CustomException(ErrorCode.ID_TOKEN_VERIFICATION_FAILED);
+        }
+    }
+
+    private void validateIssuer(OidcIdToken oidcIdToken, OauthProvider provider) {
+        String idTokenIssuer = oidcIdToken.getIssuer().toString();
+        String targetIssuer = properties.get(provider).issuer();
+
+        if (idTokenIssuer == null || !idTokenIssuer.equals(targetIssuer)) {
+            throw new CustomException(ErrorCode.ID_TOKEN_VERIFICATION_FAILED);
+        }
+    }
+
+    private Jwt getJwt(String idToken, OauthProvider provider) {
+        JwtDecoder decoder = properties.get(provider).decoder();
+        return decoder.decode(idToken);
+    }
+
+    private OidcIdToken getOidcIdToken(Jwt jwt) {
+        return new OidcIdToken(
+                jwt.getTokenValue(), jwt.getIssuedAt(), jwt.getExpiresAt(), jwt.getClaims());
+    }
+
+    private void validateNonce(OidcIdToken idToken) {
+        // TODO: 랜덤 nonce 사용하도록 개선
+        String idTokenNonceHash = idToken.getNonce();
+        String targetNonceHash = oidcProperties.nonce();
+
+        if (idTokenNonceHash == null || !idTokenNonceHash.equals(targetNonceHash)) {
+            throw new CustomException(ErrorCode.ID_TOKEN_VERIFICATION_FAILED);
+        }
+    }
+
+    private String getNonceHash(String nonce) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] digest = md.digest(nonce.getBytes(StandardCharsets.US_ASCII));
+            return Base64.getUrlEncoder().withoutPadding().encodeToString(digest);
+        } catch (NoSuchAlgorithmException e) {
+            throw new CustomException(ErrorCode.ID_TOKEN_VERIFICATION_FAILED);
+        }
+    }
+
+    record PropertyBinder(JwtDecoder decoder, String issuer, List<String> audience) {}
+}
--- src/main/java/com/depromeet/global/common/constants/SecurityConstants.java
+++ src/main/java/com/depromeet/global/common/constants/SecurityConstants.java
@@ -2,11 +2,15 @@ package com.depromeet.global.common.constants;
 
 public final class SecurityConstants {
 
-    private SecurityConstants() {}
-
     public static final String TOKEN_ROLE_NAME = "role";
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String ACCESS_TOKEN_HEADER = "Authorization";
     public static final String REFRESH_TOKEN_HEADER = "Refresh-Token";
     public static final String REGISTER_REQUIRED_HEADER = "Registration-Required";
+    public static final String KAKAO_JWK_URL = "https://kauth.kakao.com/.well-known/jwks.json";
+    public static final String APPLE_JWK_URL = "https://appleid.apple.com/auth/keys";
+    public static final String KAKAO_ISSUER = "https://kauth.kakao.com";
+    public static final String APPLE_ISSUER = "https://appleid.apple.com";
+
+    private SecurityConstants() {}
 }
