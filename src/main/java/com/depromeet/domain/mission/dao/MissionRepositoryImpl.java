28327e0fd43c2c10e9ef1282b59a1f4a341d8993
yb__char <68099546+uiurihappy@users.noreply.github.com>
2024-01-24 00:05:52 +0900

    feat: 친구 미션 목록 (#196)
    * feat: 2024.01.21 임시 커밋
    
    * feat: 친구 미션 목록 v1
    
    * fix: 엔드포인트 변경
    
    * fix: 정적 팩토리 메서드 명 변경
    
    * fix: 팔로잉 친구 미션 공개 여부 정책에 따라 수정
    
    * fix: 친구 미션 목록에서 내역으로 랜딩 시 missionRecordId가 필요하므로 of 정적 팩토리 메서드를 사용하도록 변경
    
    * fix: existsBySourceIdAndTargetId 메서드 명 변경
    
    * fix: source, target convention으로 수정
    
    * fix: spotlessApply
    
    * fix: target 대상 수정과 조건문 리팩토링

--- src/main/java/com/depromeet/domain/mission/dao/MissionRepositoryImpl.java
+++ src/main/java/com/depromeet/domain/mission/dao/MissionRepositoryImpl.java
@@ -5,6 +5,7 @@ import static com.depromeet.domain.missionRecord.domain.QMissionRecord.*;
 
 import com.depromeet.domain.mission.domain.DurationStatus;
 import com.depromeet.domain.mission.domain.Mission;
+import com.depromeet.domain.mission.domain.MissionVisibility;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -18,6 +19,7 @@ import org.springframework.stereotype.Repository;
 public class MissionRepositoryImpl implements MissionRepositoryCustom {
     private final JPAQueryFactory jpaQueryFactory;
 
+    // 내 미션 목록
     @Override
     public List<Mission> findMissionsWithRecords(Long memberId) {
         JPAQuery<Mission> query =
@@ -25,6 +27,20 @@ public class MissionRepositoryImpl implements MissionRepositoryCustom {
                         .selectFrom(mission)
                         .leftJoin(mission.missionRecords, missionRecord)
                         .where(memberIdEq(memberId))
+                        .orderBy(mission.id.desc())
+                        .fetchJoin();
+        return query.fetch();
+    }
+
+    // 친구 미션 목록
+    @Override
+    public List<Mission> findMissionsWithRecordsByRelations(
+            Long memberId, boolean existsMemberRelations) {
+        JPAQuery<Mission> query =
+                jpaQueryFactory
+                        .selectFrom(mission)
+                        .leftJoin(mission.missionRecords, missionRecord)
+                        .where(memberIdEq(memberId), visibilityByRelations(existsMemberRelations))
                         .orderBy(mission.id.desc())
                         .fetchJoin();
         return query.fetch();
@@ -43,5 +59,11 @@ public class MissionRepositoryImpl implements MissionRepositoryCustom {
 
     private BooleanExpression memberIdEq(Long memberId) {
         return memberId == null ? null : mission.member.id.eq(memberId);
+    }
+
+    private BooleanExpression visibilityByRelations(boolean existsRelations) {
+        return existsRelations
+                ? mission.visibility.in(MissionVisibility.FOLLOWER, MissionVisibility.ALL)
+                : mission.visibility.in(MissionVisibility.ALL);
     }
 }
