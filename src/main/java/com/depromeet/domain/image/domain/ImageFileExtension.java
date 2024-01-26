9198206a4da0c299f33b3bfd516f1efc4439e20e
kdomo <dongho5088@naver.com>
2024-01-26 14:29:37 +0900

    style: spotless

--- src/main/java/com/depromeet/domain/image/domain/ImageFileExtension.java
+++ src/main/java/com/depromeet/domain/image/domain/ImageFileExtension.java
@@ -1,12 +1,10 @@
 package com.depromeet.domain.image.domain;
 
-import com.depromeet.domain.auth.domain.OauthProvider;
 import com.depromeet.global.error.exception.CustomException;
 import com.depromeet.global.error.exception.ErrorCode;
+import java.util.Arrays;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-
-import java.util.Arrays;
 
 @Getter
 @AllArgsConstructor
@@ -18,10 +16,12 @@ public enum ImageFileExtension {
 
     private final String uploadExtension;
 
-            public static ImageFileExtension of(String uploadExtension) {
-            return Arrays.stream(values())
-                    .filter(imageFileExtension -> imageFileExtension.uploadExtension.equals(uploadExtension))
-                    .findFirst()
-                    .orElseThrow(() -> new CustomException(ErrorCode.IMAGE_FILE_EXTENSION_NOT_FOUND));
-        }
+    public static ImageFileExtension of(String uploadExtension) {
+        return Arrays.stream(values())
+                .filter(
+                        imageFileExtension ->
+                                imageFileExtension.uploadExtension.equals(uploadExtension))
+                .findFirst()
+                .orElseThrow(() -> new CustomException(ErrorCode.IMAGE_FILE_EXTENSION_NOT_FOUND));
+    }
 }
