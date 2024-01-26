14f77e9fcac338c2456b2c3df6685a11ddd574a8
kdomo <dongho5088@naver.com>
2024-01-26 14:29:19 +0900

    hotfix: 회원 정보 조회 시 ImageFileExtension도 같이 응답하도록 수정

--- src/main/java/com/depromeet/domain/image/domain/ImageFileExtension.java
+++ src/main/java/com/depromeet/domain/image/domain/ImageFileExtension.java
@@ -1,7 +1,12 @@
 package com.depromeet.domain.image.domain;
 
+import com.depromeet.domain.auth.domain.OauthProvider;
+import com.depromeet.global.error.exception.CustomException;
+import com.depromeet.global.error.exception.ErrorCode;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.util.Arrays;
 
 @Getter
 @AllArgsConstructor
@@ -12,4 +17,11 @@ public enum ImageFileExtension {
     ;
 
     private final String uploadExtension;
+
+            public static ImageFileExtension of(String uploadExtension) {
+            return Arrays.stream(values())
+                    .filter(imageFileExtension -> imageFileExtension.uploadExtension.equals(uploadExtension))
+                    .findFirst()
+                    .orElseThrow(() -> new CustomException(ErrorCode.IMAGE_FILE_EXTENSION_NOT_FOUND));
+        }
 }
