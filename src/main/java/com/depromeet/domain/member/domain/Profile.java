43b32f336c5872c21942f80efd69d6e7ecab7624
도모 <dongho5088@naver.com>
2024-01-26 14:26:55 +0900

    refactor: Image 업로드 시 ImageKey 관리 (#226)
    * feat: Image Domain 생성
    
    * refactor: image 업로드 시 키 생성과 조회 시 CDN주소로 설정
    
    * refactor: profile get timestamp 제거
    
    * test: 테스트 코드 수정
    
    * style: spotless
    
    * fix: image Key length 36으로 변경

--- src/main/java/com/depromeet/domain/member/domain/Profile.java
+++ src/main/java/com/depromeet/domain/member/domain/Profile.java
@@ -8,7 +8,7 @@ import lombok.*;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Profile {
     private String nickname;
-    private String profileImageUrl;
+    @Getter private String profileImageUrl;
 
     @Builder(access = AccessLevel.PRIVATE)
     private Profile(String nickname, String profileImageUrl) {
@@ -18,13 +18,5 @@ public class Profile {
 
     public static Profile createProfile(String nickname, String profileImageUrl) {
         return Profile.builder().nickname(nickname).profileImageUrl(profileImageUrl).build();
-    }
-
-    // TODO: 이미지 업로드 로직 개선후 timestamp 제거
-    public String getProfileImageUrl() {
-        if (profileImageUrl == null) {
-            return null;
-        }
-        return profileImageUrl + "?timestamp=" + System.currentTimeMillis();
     }
 }
