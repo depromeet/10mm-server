818d7b01382fb0c247ac9ccfcb529942adaa313f
uwoobeat <desktop3711@gmail.com>
2024-01-29 01:07:17 +0900

    refactor: 멤버 테스트 수정

--- src/test/java/com/depromeet/domain/member/domain/MemberTest.java
+++ src/test/java/com/depromeet/domain/member/domain/MemberTest.java
@@ -1,28 +1,16 @@
 package com.depromeet.domain.member.domain;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import com.depromeet.global.error.exception.CustomException;
-import com.depromeet.global.error.exception.ErrorCode;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class MemberTest {
 
-    // Fixture
-    Profile profile;
-
-    @BeforeEach
-    void setUp() {
-        profile = Profile.createProfile("testNickname", "testProfileImageUrl");
-    }
-
     @Test
-    void 회원가입시_초기_상태는_NORMAL이다() {
+    void 소셜_로그인시_초기_상태는_NORMAL이다() {
         // given
         Member member =
-                Member.createGuestMember(
+                Member.createNormalMember(
                         OauthInfo.createOauthInfo("testProvider", "testProviderId", "testEmail"),
                         "testNickname");
 
@@ -34,10 +22,10 @@ class MemberTest {
     }
 
     @Test
-    void 회원가입시_초기_역할은_GUEST이다() {
+    void 소셜_로그인시_초기_역할은_USER이다() {
         // given
         Member member =
-                Member.createGuestMember(
+                Member.createNormalMember(
                         OauthInfo.createOauthInfo("testProvider", "testProviderId", "testEmail"),
                         "testNickname");
 
@@ -45,14 +33,14 @@ class MemberTest {
         MemberRole role = member.getRole();
 
         // then
-        assertEquals(MemberRole.GUEST, role);
+        assertEquals(MemberRole.USER, role);
     }
 
     @Test
-    void 회원가입시_초기_공개여부는_PUBLIC이다() {
+    void 소셜_로그인시시_초기_계정공개여부는_PUBLIC이다() {
         // given
         Member member =
-                Member.createGuestMember(
+                Member.createNormalMember(
                         OauthInfo.createOauthInfo("testProvider", "testProviderId", "testEmail"),
                         "testNickname");
 
@@ -61,47 +49,6 @@ class MemberTest {
 
         // then
         assertEquals(MemberVisibility.PUBLIC, visibility);
-    }
-
-    @Test
-    void 회원가입시_게스트멤버의_닉네임이_설정된다() {
-        // given
-        Member member =
-                Member.createGuestMember(
-                        OauthInfo.createOauthInfo("testProvider", "testProviderId", "testEmail"),
-                        "testNickname");
-
-        // when
-        member.register("testNickname");
-
-        // then
-        assertEquals("testNickname", member.getProfile().getNickname());
-    }
-
-    @Test
-    void 회원가입시_게스트멤버는_일반멤버로_변경된다() {
-        // given
-        Member member =
-                Member.createGuestMember(
-                        OauthInfo.createOauthInfo("testProvider", "testProviderId", "testEmail"),
-                        "testNickname");
-
-        // when
-        member.register("testNickname");
-
-        // then
-        assertEquals(MemberRole.USER, member.getRole());
-    }
-
-    @Test
-    void 회원가입시_일반멤버이면_예외가_발생한다() {
-        // given
-        Member member = Member.createNormalMember(profile);
-
-        // when & then
-        assertThatThrownBy(() -> member.register("testNickname"))
-                .isInstanceOf(CustomException.class)
-                .hasMessage(ErrorCode.MEMBER_ALREADY_REGISTERED.getMessage());
     }
 
     @Test
