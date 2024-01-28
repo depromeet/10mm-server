917d1f75f3f0f093154527b9af41795fad042c0f
Jaehyun Ahn <91878695+uwoobeat@users.noreply.github.com>
2024-01-29 01:43:59 +0900

    feat: 랜덤 닉네임 기능 도입에 따른 `GUEST` 상태 제거 (#245)
    * refactor: 멤버 테스트 수정
    
    * feat: 회원가입 메서드 삭제
    
    * feat: 게스트 로직 삭제
    
    * feat: ID/PW 관련 API deprecated 처리
    
    * feat: Profile로 멤버 생성하는 메서드 deprecated 처리
    
    * feat ID/PW 로직 관련 메서드 일부 제거
    
    * refactor: 소셜 로그인 응답의 게스트 여부 필드가 항상 false이도록 수정
    
    * style: spotless 적용
    
    * refactor: guest 관련 변수명, 메서드명 수정

--- src/main/java/com/depromeet/global/error/exception/ErrorCode.java
+++ src/main/java/com/depromeet/global/error/exception/ErrorCode.java
@@ -30,7 +30,6 @@ public enum ErrorCode {
     MEMBER_ALREADY_NICKNAME(HttpStatus.CONFLICT, "이미 존재하는 닉네임입니다."),
     MEMBER_ALREADY_DELETED(HttpStatus.NOT_FOUND, "이미 탈퇴한 회원입니다."),
     PASSWORD_NOT_MATCHES(HttpStatus.UNAUTHORIZED, "비밀번호가 일치하지 않습니다."),
-    GUEST_MEMBER_REQUIRES_REGISTRATION(HttpStatus.UNAUTHORIZED, "게스트 회원은 회원가입을 먼저 진행해야 합니다."),
     ID_TOKEN_VERIFICATION_FAILED(HttpStatus.UNAUTHORIZED, "ID 토큰 검증에 실패했습니다."),
 
     // Mission
