bc0e7c6162d933d46f0f58bbb38821c63269c38a
uwoobeat <desktop3711@gmail.com>
2024-01-29 22:50:53 +0900

    feat: 토큰 DTO 파싱 및 조회 로직 추가

--- src/main/java/com/depromeet/domain/auth/application/JwtTokenService.java
+++ src/main/java/com/depromeet/domain/auth/application/JwtTokenService.java
@@ -63,6 +63,46 @@ public class JwtTokenService {
         refreshTokenRepository.save(refreshToken);
     }
 
+    public AccessTokenDto retrieveAccessToken(String accessTokenValue) {
+        try {
+            return jwtTokenProvider.parseAccessToken(accessTokenValue);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public RefreshTokenDto retrieveRefreshToken(String refreshTokenValue) {
+        RefreshTokenDto refreshTokenDto = parseRefreshToken(refreshTokenValue);
+
+        if (refreshTokenDto == null) {
+            return null;
+        }
+
+        // 파싱된 DTO와 일치하는 토큰이 Redis에 저장되어 있는지 확인
+        Optional<RefreshToken> refreshToken = getRefreshTokenFromRedis(refreshTokenDto.memberId());
+
+        // Redis에 토큰이 존재하고, 쿠키의 토큰과 값이 일치하면 DTO 반환
+        if (refreshToken.isPresent()
+                && refreshTokenDto.tokenValue().equals(refreshToken.get().getToken())) {
+            return refreshTokenDto;
+        }
+
+        // Redis에 토큰이 존재하지 않거나, 쿠키의 토큰과 값이 일치하지 않으면 null 반환
+        return null;
+    }
+
+    private Optional<RefreshToken> getRefreshTokenFromRedis(Long memberId) {
+        return refreshTokenRepository.findById(memberId);
+    }
+
+    private RefreshTokenDto parseRefreshToken(String refreshTokenValue) {
+        try {
+            return jwtTokenProvider.parseRefreshToken(refreshTokenValue);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
     public String reissueAccessToken(String refreshToken) {
         Member member = getMemberFrom(refreshToken);
         return createAccessToken(member.getId(), member.getRole());
