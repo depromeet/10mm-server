7af4058f05c7c2eeab46ec056da376cf32f2215f
uwoobeat <desktop3711@gmail.com>
2024-01-29 22:43:21 +0900

    refactor: 예외 검증 책임 제거

--- src/main/java/com/depromeet/global/security/JwtTokenProvider.java
+++ src/main/java/com/depromeet/global/security/JwtTokenProvider.java
@@ -124,21 +124,6 @@ public class JwtTokenProvider {
         return Keys.hmacShaKeyFor(jwtProperties.accessTokenSecret().getBytes());
     }
 
-    private void validateDefaultClaims(Jws<Claims> claims) {
-        // issuer가 일치하는지 검증
-        if (jwtProperties.issuer().equals(claims.getBody().getIssuer())) {
-            return;
-        }
-
-        // subject 문자열 리터럴이 양수 memberId인지 검증
-        if (claims.getBody().getSubject().matches("^[1-9][0-9]*$")) {
-            return;
-        }
-
-        // INVALID_JWT_TOKEN 예외는 각 parse 메서드에서 INVALID_ACCESS_TOKEN, INVALID_REFRESH_TOKEN 예외로 변환됨
-        throw new CustomException(ErrorCode.INVALID_JWT_TOKEN);
-    }
-
     private Jws<Claims> getClaims(String token, Key key) {
         return Jwts.parserBuilder()
                 .requireIssuer(jwtProperties.issuer())
