9ff99236f81daba289fe2f894cb9a0f8b4720e65
ybchar <uiurihappy@sju.ac.kr>
2024-01-29 19:42:25 +0900

    feat: FCM Config 추가

---
+++ src/main/java/com/depromeet/global/config/fcm/FcmConfig.java
@@ -0,0 +1,37 @@
+package com.depromeet.global.config.fcm;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import javax.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FcmConfig {
+
+    @Value("${fcm.certification}")
+    private String fcmCertification;
+
+    @PostConstruct
+    public void init() {
+        try {
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseOptions options =
+                        new FirebaseOptions.Builder()
+                                .setCredentials(
+                                        GoogleCredentials.fromStream(
+                                                new ByteArrayInputStream(
+                                                        fcmCertification.getBytes(
+                                                                StandardCharsets.UTF_8))))
+                                .build();
+                FirebaseApp.initializeApp(options);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/depromeet/global/config/fcm/FcmService.java
@@ -0,0 +1,45 @@
+package com.depromeet.global.config.fcm;
+
+import com.google.api.core.ApiFuture;
+import com.google.firebase.messaging.*;
+import java.util.List;
+import java.util.Map;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FcmService {
+
+    /**
+     * https://firebase.google.com/support/release-notes/admin/java
+	 * 위 레퍼런스에 의거하여 sendMulticastAsync 는 Deprecated 되어 sendEachForMulticastAsync
+     *
+     * @param tokenList: 푸시 토큰 리스트
+     * @param title: 알림 제목
+     * @param content: 알림 내용
+     * @param data: 푸시 데이터
+     * @return ApiFuture<BatchResponse>
+     */
+    public ApiFuture<BatchResponse> sendGroupMessageAsync(
+            List<String> tokenList, String title, String content, Map<String, String> data) {
+        MulticastMessage multicast =
+                MulticastMessage.builder()
+                        .addAllTokens(tokenList)
+                        .setNotification(
+                                Notification.builder().setTitle(title).setBody(content).build())
+                        .putAllData(data)
+                        .build();
+        return FirebaseMessaging.getInstance().sendEachForMulticastAsync(multicast);
+    }
+
+    public ApiFuture<String> sendMessageSync(
+            String token, String title, String content, Map<String, String> data) {
+        Message message =
+                Message.builder()
+                        .setToken(token)
+                        .setNotification(
+                                Notification.builder().setTitle(title).setBody(content).build())
+                        .putAllData(data)
+                        .build();
+        return FirebaseMessaging.getInstance().sendAsync(message);
+    }
+}
