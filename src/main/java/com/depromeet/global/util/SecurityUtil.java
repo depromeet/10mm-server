835c062e7ac7e782fc027e50bb2d4168d7723fd4
uwoobeat <desktop3711@gmail.com>
2024-01-31 01:13:08 +0900

    fix: 캐스팅 대신 인터페이스 메서드 사용하도록 수정

--- src/main/java/com/depromeet/global/util/SecurityUtil.java
+++ src/main/java/com/depromeet/global/util/SecurityUtil.java
@@ -1,6 +1,6 @@
 package com.depromeet.global.util;
 
-import com.depromeet.global.security.PrincipalDetails;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
@@ -8,9 +8,7 @@ import org.springframework.stereotype.Component;
 public class SecurityUtil {
 
     public Long getCurrentMemberId() {
-        PrincipalDetails principal =
-                (PrincipalDetails)
-                        SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        return Long.parseLong(principal.getUsername());
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return Long.parseLong(authentication.getName());
     }
 }
