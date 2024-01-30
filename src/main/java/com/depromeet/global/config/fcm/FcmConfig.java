5d0ed71351778bc7a21bfd74dd2c33c3c04b9c5f
ybchar <uiurihappy@sju.ac.kr>
2024-01-31 02:36:26 +0900

    fix: PostConstruct javax -> jakarta

--- src/main/java/com/depromeet/global/config/fcm/FcmConfig.java
+++ src/main/java/com/depromeet/global/config/fcm/FcmConfig.java
@@ -3,9 +3,9 @@ package com.depromeet.global.config.fcm;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import jakarta.annotation.PostConstruct;
 import java.io.ByteArrayInputStream;
 import java.nio.charset.StandardCharsets;
-import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
