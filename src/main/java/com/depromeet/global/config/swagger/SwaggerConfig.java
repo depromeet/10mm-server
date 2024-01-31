197be9d75e520e5cca8c446afc9f4e3080ebe66f
uwoobeat <desktop3711@gmail.com>
2024-01-31 14:01:33 +0900

    feat: 스웨거 API 요청 시 엑세스 토큰 헤더에 포함되도록 설정 변경

--- src/main/java/com/depromeet/global/config/swagger/SwaggerConfig.java
+++ src/main/java/com/depromeet/global/config/swagger/SwaggerConfig.java
@@ -10,6 +10,7 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.security.SecurityScheme.In;
 import io.swagger.v3.oas.models.security.SecurityScheme.Type;
@@ -37,6 +38,7 @@ public class SwaggerConfig {
     public OpenAPI openAPI() {
         return new OpenAPI()
                 .servers(swaggerServers())
+                .addSecurityItem(securityRequirement())
                 .components(authSetting())
                 .info(swaggerInfo());
     }
@@ -79,6 +81,12 @@ public class SwaggerConfig {
                 .title(API_TITLE)
                 .description(API_DESCRIPTION)
                 .license(license);
+    }
+
+    private SecurityRequirement securityRequirement() {
+        SecurityRequirement securityRequirement = new SecurityRequirement();
+        securityRequirement.addList("accessToken");
+        return securityRequirement;
     }
 
     @Bean
