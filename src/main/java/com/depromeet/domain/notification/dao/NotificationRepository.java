ae67769948cba0cba8be362b2f1037510a360732
kdomo <dongho5088@naver.com>
2024-02-07 13:48:27 +0900

    fix: 팔로우 취소 시 알림 히스토리 삭제

--- src/main/java/com/depromeet/domain/notification/dao/NotificationRepository.java
+++ src/main/java/com/depromeet/domain/notification/dao/NotificationRepository.java
@@ -1,6 +1,10 @@
 package com.depromeet.domain.notification.dao;
 
 import com.depromeet.domain.notification.domain.Notification;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface NotificationRepository extends JpaRepository<Notification, Long> {}
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    Optional<Notification> findBySourceMemberIdAndTargetMemberId(
+            Long sourceMemberId, Long targetMemberId);
+}
