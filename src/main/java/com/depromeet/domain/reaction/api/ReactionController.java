05fce73204bfaeea6c1c34e0ddb0e893ffec0979
Jaehyun Ahn <desktop3711@gmail.com>
2024-02-07 21:17:26 +0900

    feat: 리액션 삭제 API 구현

--- src/main/java/com/depromeet/domain/reaction/api/ReactionController.java
+++ src/main/java/com/depromeet/domain/reaction/api/ReactionController.java
@@ -9,6 +9,8 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,5 +30,12 @@ public class ReactionController {
             @Valid @RequestBody ReactionCreateRequest request) {
         ReactionCreateResponse response = reactionService.createReaction(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @Operation(summary = "리액션 삭제", description = "미션 기록에서 리액션을 삭제합니다.")
+    @DeleteMapping("/{reactionId}")
+    public ResponseEntity<Void> reactionDelete(@PathVariable Long reactionId) {
+        reactionService.deleteReaction(reactionId);
+        return ResponseEntity.noContent().build();
     }
 }
