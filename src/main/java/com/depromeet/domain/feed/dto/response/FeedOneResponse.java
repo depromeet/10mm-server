0fe363488cdacb474d76437d891f65fc0b9b83b4
yb__char <68099546+uiurihappy@users.noreply.github.com>
2024-02-06 23:43:22 -0800

    feat: 피드 탭 구현 (#277)
    * feat: feed 탭 리스트 구현
    
    * feat: 프로필 피드 탭 구현
    
    * feat: 피드 탭 닉네임 추가
    
    * feat: 피드 탭 v2
    
    * fix: 마이프로필 피드 공개 여부 예외 처리
    
    * refactor: FeedOneResponse class -> record
    
    * refactor: feed/me API 개선
    
    * refactor: 피드 탭 리스트 limit 100 제한
    
    * fix: 리뷰 반영
    
    * fix: targetId -> memberId
    
    * fix: 타인 프로필 공개여부 조건 추가
    
    * fix: Transactional 추가
    
    * fix: getter id가 아닌 member 프록시 객체 리스트로 수정
    
    * fix: @uwoobeat 리뷰 반영
    
    * fix: 주석 삭제

---
+++ src/main/java/com/depromeet/domain/feed/dto/response/FeedOneResponse.java
@@ -0,0 +1,96 @@
+package com.depromeet.domain.feed.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.querydsl.core.annotations.QueryProjection;
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+public record FeedOneResponse(
+        @Schema(description = "작성자 ID", defaultValue = "1") Long memberId,
+        @Schema(description = "작성자 닉네임", defaultValue = "default name") String nickname,
+        @Schema(description = "작성자 프로필 이미지", defaultValue = "https://image.10mm.today/default.png")
+                String profileImage,
+        @Schema(description = "미션 ID", defaultValue = "1") Long missionId,
+        @Schema(description = "미션 이름", defaultValue = "default name") String name,
+        @Schema(description = "미션 기록 ID", defaultValue = "1") Long recordId,
+        @Schema(description = "미션 일지 내용", defaultValue = "default remark") String remark,
+        @Schema(
+                        description = "미션 기록 인증 사진 Url",
+                        defaultValue = "https://image.10mm.today/default.png")
+                String recordImageUrl,
+        @Schema(description = "미션 수행한 시간", defaultValue = "21") long duration,
+        @Schema(description = "미션 시작한 지 N일차", defaultValue = "3") long sinceDay,
+        @JsonFormat(
+                        shape = JsonFormat.Shape.STRING,
+                        pattern = "yyyy-MM-dd HH:mm:ss",
+                        timezone = "Asia/Seoul")
+                @Schema(
+                        description = "미션 기록 시작 시간",
+                        defaultValue = "2024-01-06 00:00:00",
+                        type = "string")
+                LocalDateTime startedAt,
+        @JsonFormat(
+                        shape = JsonFormat.Shape.STRING,
+                        pattern = "yyyy-MM-dd HH:mm:ss",
+                        timezone = "Asia/Seoul")
+                @Schema(
+                        description = "미션 기록 종료 시간",
+                        defaultValue = "2024-01-20 00:34:00",
+                        type = "string")
+                LocalDateTime finishedAt) {
+    @QueryProjection
+    public FeedOneResponse(
+            Long memberId,
+            String nickname,
+            String profileImage,
+            Long missionId,
+            String name,
+            Long recordId,
+            String remark,
+            String recordImageUrl,
+            Duration duration,
+            LocalDateTime startedAt,
+            LocalDateTime finishedAt) {
+        this(
+                memberId,
+                nickname,
+                profileImage,
+                missionId,
+                name,
+                recordId,
+                remark,
+                recordImageUrl,
+                duration.toMinutes(),
+                Duration.between(startedAt, LocalDateTime.now()).toDays() + 1,
+                startedAt,
+                finishedAt);
+    }
+
+    public static FeedOneResponse of(
+            Long memberId,
+            String nickname,
+            String profileImage,
+            Long missionId,
+            String name,
+            Long recordId,
+            String remark,
+            String recordImageUrl,
+            Duration duration,
+            LocalDateTime startedAt,
+            LocalDateTime finishedAt) {
+        return new FeedOneResponse(
+                memberId,
+                nickname,
+                profileImage,
+                missionId,
+                name,
+                recordId,
+                remark,
+                recordImageUrl,
+                duration.toMinutes(),
+                Duration.between(startedAt, LocalDateTime.now()).toDays() + 1,
+                startedAt,
+                finishedAt);
+    }
+}
