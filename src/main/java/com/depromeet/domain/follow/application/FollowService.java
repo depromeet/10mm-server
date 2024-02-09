42559665471fb249b96044da8aeb7964265723b3
도모 <dongho5088@naver.com>
2024-02-09 16:18:46 +0900

    feat: 팔로우 취소 시 상대의 팔로우 상태 응답하도록 변경 (#296)
    * feat: 팔로우 취소 시 상대의 팔로우 상태 응답 하도록 변경
    
    * test: 팔로우 취소 시 follow Status 테스트 코드 추가

--- src/main/java/com/depromeet/domain/follow/application/FollowService.java
+++ src/main/java/com/depromeet/domain/follow/application/FollowService.java
@@ -62,7 +62,7 @@ public class FollowService {
         memberRelationRepository.save(memberRelation);
     }
 
-    public void deleteFollow(FollowDeleteRequest request) {
+    public FollowerDeletedResponse deleteFollow(FollowDeleteRequest request) {
         final Member currentMember = memberUtil.getCurrentMember();
         Member targetMember = getTargetMember(request.targetId());
 
@@ -79,6 +79,14 @@ public class FollowService {
             notificationRepository.delete(notification);
         }
         memberRelationRepository.delete(memberRelation);
+
+        Optional<MemberRelation> optionalMemberRelation =
+                memberRelationRepository.findBySourceIdAndTargetId(
+                        targetMember.getId(), currentMember.getId());
+
+        return optionalMemberRelation.isPresent()
+                ? FollowerDeletedResponse.from(FollowStatus.FOLLOWED_BY_ME)
+                : FollowerDeletedResponse.from(FollowStatus.NOT_FOLLOWING);
     }
 
     @Transactional(readOnly = true)
