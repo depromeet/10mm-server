9f11b1bea8ac8ddb79806547a58177d27299ea61
kdomo <dongho5088@naver.com>
2024-02-12 19:29:45 +0900

    fix: isCompletedMissionToday에 COMPLETE 비교 추가

--- src/main/java/com/depromeet/domain/mission/domain/Mission.java
+++ src/main/java/com/depromeet/domain/mission/domain/Mission.java
@@ -2,6 +2,7 @@ package com.depromeet.domain.mission.domain;
 
 import com.depromeet.domain.common.model.BaseTimeEntity;
 import com.depromeet.domain.member.domain.Member;
+import com.depromeet.domain.missionRecord.domain.ImageUploadStatus;
 import com.depromeet.domain.missionRecord.domain.MissionRecord;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
@@ -128,7 +129,9 @@ public class Mission extends BaseTimeEntity {
                         record ->
                                 record.getStartedAt()
                                         .toLocalDate()
-                                        .equals(LocalDateTime.now().toLocalDate()))
+                                        .equals(LocalDateTime.now().toLocalDate())
+                                        && record.getUploadStatus() == ImageUploadStatus.COMPLETE
+                )
                 .findFirst()
                 .isPresent();
     }
