a44516a34fa45301834815d503c1f375f930f63e
ybchar <uiurihappy@sju.ac.kr>
2024-02-12 14:54:51 +0900

    feat: 미션별 통계 API 테스트 케이스 추가

--- src/test/java/com/depromeet/domain/missionRecord/application/MissionRecordServiceTest.java
+++ src/test/java/com/depromeet/domain/missionRecord/application/MissionRecordServiceTest.java
@@ -11,9 +11,13 @@ import com.depromeet.domain.mission.dao.MissionRepository;
 import com.depromeet.domain.mission.domain.Mission;
 import com.depromeet.domain.mission.domain.MissionCategory;
 import com.depromeet.domain.mission.domain.MissionVisibility;
+import com.depromeet.domain.missionRecord.dao.MissionRecordRepository;
+import com.depromeet.domain.missionRecord.domain.MissionRecord;
 import com.depromeet.domain.missionRecord.dto.request.MissionRecordCreateRequest;
+import com.depromeet.domain.missionRecord.dto.response.MissionStatisticsResponse;
 import com.depromeet.global.error.exception.CustomException;
 import com.depromeet.global.util.SecurityUtil;
+import java.time.Duration;
 import java.time.LocalDateTime;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -31,12 +35,14 @@ class MissionRecordServiceTest {
     @Autowired MissionService missionService;
     @Autowired MemberRepository memberRepository;
     @Autowired MissionRepository missionRepository;
+    @Autowired MissionRecordRepository missionRecordRepository;
     @Autowired DatabaseCleaner databaseCleaner;
     @MockBean SecurityUtil securityUtil;
 
     private Member member;
     private Mission mission;
-    private LocalDateTime now = LocalDateTime.now();
+    private final LocalDateTime now = LocalDateTime.now();
+    private final LocalDateTime missionStartedAt = LocalDateTime.of(2024, 1, 24, 22, 58, 53);
 
     @BeforeEach
     void setUp() {
@@ -47,7 +53,6 @@ class MissionRecordServiceTest {
                 Member.createNormalMember(
                         OauthInfo.createOauthInfo("test", "test", "test"), "test");
         memberRepository.save(member);
-
         mission =
                 Mission.createMission(
                         "test",
@@ -55,8 +60,8 @@ class MissionRecordServiceTest {
                         1,
                         MissionCategory.ETC,
                         MissionVisibility.ALL,
-                        now,
-                        now.plusWeeks(2),
+                        missionStartedAt,
+                        missionStartedAt.plusWeeks(2),
                         member);
         missionRepository.save(mission);
     }
@@ -75,5 +80,52 @@ class MissionRecordServiceTest {
 
         Assertions.assertThrows(
                 CustomException.class, () -> missionRecordService.findOneMissionRecord(missionId));
+    }
+
+    @Test
+    void 미션별_상세_통계_조회한다() {
+        // given
+        LocalDateTime recordStartedAt = LocalDateTime.of(2024, 1, 25, 22, 58, 53);
+        LocalDateTime recordFinishedAt = recordStartedAt.plusMinutes(17);
+        // 연속 성공 3일
+        for (int i = 0; i < 3; i++) {
+            MissionRecord missionRecord =
+                    MissionRecord.createMissionRecord(
+                            Duration.ofMinutes(17),
+                            recordStartedAt.plusDays(i),
+                            recordFinishedAt.plusDays(i),
+                            mission);
+            missionRecord.updateUploadStatusPending();
+            missionRecord.updateUploadStatusComplete(
+                    "testRemark " + i, "https://image.10mm.today/default.png");
+            missionRecordRepository.save(missionRecord);
+        }
+
+        // 연속 성공 4일
+        for (int i = 4; i < 8; i++) {
+            MissionRecord missionRecord =
+                    MissionRecord.createMissionRecord(
+                            Duration.ofMinutes(17),
+                            recordStartedAt.plusDays(i),
+                            recordFinishedAt.plusDays(i),
+                            mission);
+            missionRecord.updateUploadStatusPending();
+            missionRecord.updateUploadStatusComplete(
+                    "testRemark " + i, "https://image.10mm.today/default.png");
+            missionRecordRepository.save(missionRecord);
+        }
+
+        // when
+        MissionStatisticsResponse missionStatistics =
+                missionRecordService.findMissionStatistics(mission.getId());
+
+        // then
+        Assertions.assertEquals(missionStatistics.totalMissionHour(), 1);
+        Assertions.assertEquals(missionStatistics.totalMissionMinute(), 59);
+        Assertions.assertEquals(missionStatistics.totalSymbolStack(), 7);
+        Assertions.assertEquals(missionStatistics.continuousSuccessDay(), 4);
+        Assertions.assertEquals(missionStatistics.totalSuccessDay(), 7);
+        Assertions.assertEquals(missionStatistics.totalMissionAttainRate(), 46.7);
+        Assertions.assertEquals(missionStatistics.timeTable().size(), 7);
     }
 }
