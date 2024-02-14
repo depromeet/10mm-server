940eb0ddb3b287129de45a494db2028d6b716510
kdomo <dongho5088@naver.com>
2024-02-14 17:31:38 +0900

    fix: notification 저장 로직 제거

--- src/main/java/com/depromeet/domain/reaction/application/ReactionService.java
+++ src/main/java/com/depromeet/domain/reaction/application/ReactionService.java
@@ -7,9 +7,6 @@ import com.depromeet.domain.member.domain.Member;
 import com.depromeet.domain.missionRecord.dao.MissionRecordRepository;
 import com.depromeet.domain.missionRecord.domain.MissionRecord;
 import com.depromeet.domain.notification.application.FcmService;
-import com.depromeet.domain.notification.dao.NotificationRepository;
-import com.depromeet.domain.notification.domain.Notification;
-import com.depromeet.domain.notification.domain.NotificationType;
 import com.depromeet.domain.reaction.dao.ReactionRepository;
 import com.depromeet.domain.reaction.domain.EmojiType;
 import com.depromeet.domain.reaction.domain.Reaction;
@@ -36,7 +33,6 @@ public class ReactionService {
     private final MissionRecordRepository missionRecordRepository;
     private final ReactionRepository reactionRepository;
     private final FcmService fcmService;
-    private final NotificationRepository notificationRepository;
 
     public List<ReactionGroupByEmojiResponse> findAllReaction(Long missionRecordId) {
         Map<EmojiType, List<Reaction>> reactionMap =
@@ -67,9 +63,6 @@ public class ReactionService {
                         PUSH_REACTION_CONTENT,
                         member.getProfile().getNickname(),
                         missionRecord.getMission().getName()));
-        Notification notification =
-                Notification.createNotification(NotificationType.REACTION, member, targetMember);
-        notificationRepository.save(notification);
         return ReactionCreateResponse.from(reactionRepository.save(reaction));
     }
 
