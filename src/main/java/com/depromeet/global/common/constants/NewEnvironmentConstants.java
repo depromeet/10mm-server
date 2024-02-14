b79282c0df16a01fa5b1db78f86d02e0fdff2dd6
uwoobeat <desktop3711@gmail.com>
2024-02-15 02:46:22 +0900

    refactor: 환경변수 상수 클래스를 enum 클래스로 변경

---
+++ src/main/java/com/depromeet/global/common/constants/NewEnvironmentConstants.java
@@ -0,0 +1,27 @@
+package com.depromeet.global.common.constants;
+
+import static com.depromeet.global.common.constants.NewEnvironmentConstants.Constants.*;
+
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+public enum NewEnvironmentConstants {
+    PROD(PROD_ENV),
+    DEV(DEV_ENV),
+    LOCAL(LOCAL_ENV);
+
+    private final String value;
+
+    @NoArgsConstructor(access = AccessLevel.PRIVATE)
+    public static class Constants {
+        public static final String PROD_ENV = "prod";
+        public static final String DEV_ENV = "dev";
+        public static final String LOCAL_ENV = "local";
+        public static final List<String> PROD_AND_DEV_ENV = List.of(PROD_ENV, DEV_ENV);
+    }
+}
