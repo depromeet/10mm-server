b9c2d9c04c5eb39c629443caf14adf8a8d65a2d3
Jaehyun Ahn <desktop3711@gmail.com>
2024-03-25 21:43:35 +0900

    feat: 랜딩 상태 결정하는 로직 추가

--- src/main/java/com/depromeet/domain/auth/dto/response/SocialLoginResponse.java
+++ src/main/java/com/depromeet/domain/auth/dto/response/SocialLoginResponse.java
@@ -1,5 +1,6 @@
 package com.depromeet.domain.auth.dto.response;
 
+import com.depromeet.domain.auth.domain.LandingStatus;
 import com.depromeet.domain.member.domain.Member;
 import io.swagger.v3.oas.annotations.media.Schema;
 
@@ -7,13 +8,16 @@ public record SocialLoginResponse(
         @Schema(description = "멤버 ID", defaultValue = "1") Long memberId,
         @Schema(description = "엑세스 토큰", defaultValue = "accessToken") String accessToken,
         @Schema(description = "리프레시 토큰", defaultValue = "refreshToken") String refreshToken,
-        @Schema(description = "게스트 여부", defaultValue = "false") boolean isGuest) {
+        @Schema(description = "게스트 여부", defaultValue = "false") boolean isGuest,
+        @Schema(description = "랜딩 상태", defaultValue = "TO_MAIN") LandingStatus landingStatus) {
 
-    public static SocialLoginResponse from(Member member, TokenPairResponse tokenPairResponse) {
+    public static SocialLoginResponse of(
+            Member member, TokenPairResponse tokenPairResponse, LandingStatus landingStatus) {
         return new SocialLoginResponse(
                 member.getId(),
                 tokenPairResponse.accessToken(),
                 tokenPairResponse.refreshToken(),
-                false);
+                false,
+                landingStatus);
     }
 }
