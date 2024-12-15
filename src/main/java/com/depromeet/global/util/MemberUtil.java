34e4d3c9afb2550793f521c7a3f356a5f01e8146
yb__char <uiurihappy@naver.com>
2024-12-15 21:03:46 +0900

    refactor: Virtual Thread Example

--- src/main/java/com/depromeet/global/util/MemberUtil.java
+++ src/main/java/com/depromeet/global/util/MemberUtil.java
@@ -4,6 +4,9 @@ import com.depromeet.domain.member.dao.MemberRepository;
 import com.depromeet.domain.member.domain.Member;
 import com.depromeet.global.error.exception.CustomException;
 import com.depromeet.global.error.exception.ErrorCode;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -13,16 +16,30 @@ public class MemberUtil {
 
     private final SecurityUtil securityUtil;
     private final MemberRepository memberRepository;
+    private static final ExecutorService VIRTUAL_THREAD_EXECUTOR =
+            Executors.newVirtualThreadPerTaskExecutor();
 
     public Member getCurrentMember() {
-        return memberRepository
-                .findById(securityUtil.getCurrentMemberId())
-                .orElseThrow(() -> new CustomException(ErrorCode.MEMBER_NOT_FOUND));
+        return CompletableFuture.supplyAsync(
+                        () ->
+                                memberRepository
+                                        .findById(securityUtil.getCurrentMemberId())
+                                        .orElseThrow(
+                                                () ->
+                                                        new CustomException(
+                                                                ErrorCode.MEMBER_NOT_FOUND)))
+                .join();
     }
 
     public Member getMemberByMemberId(Long memberId) {
-        return memberRepository
-                .findById(memberId)
-                .orElseThrow(() -> new CustomException(ErrorCode.MEMBER_NOT_FOUND));
+        return CompletableFuture.supplyAsync(
+                        () ->
+                                memberRepository
+                                        .findById(memberId)
+                                        .orElseThrow(
+                                                () ->
+                                                        new CustomException(
+                                                                ErrorCode.MEMBER_NOT_FOUND)))
+                .join();
     }
 }
