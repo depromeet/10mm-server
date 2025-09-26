518f0047df384dac4921c19a20943264bce5cf19
yb__char <68099546+char-yb@users.noreply.github.com>
2025-09-26 21:08:24 +0900

    fix: Remove presignedUrl ACL (#441)
    * fix: remove presignedUrl ACL
    
    * fix: remove legacy createPresignedUrl

--- src/main/java/com/depromeet/global/util/ImageUtil.java
+++ src/main/java/com/depromeet/global/util/ImageUtil.java
@@ -2,12 +2,13 @@ package com.depromeet.global.util;
 
 import com.depromeet.domain.image.domain.ImageFileExtension;
 import com.depromeet.infra.config.s3.S3Properties;
+import java.net.URL;
 import java.time.Duration;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-import software.amazon.awssdk.services.s3.model.ObjectCannedACL;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
 
@@ -20,18 +21,24 @@ public class ImageUtil {
     private final S3Properties s3Properties;
 
     @SneakyThrows
-    public String createPreSignedUrl(String fileName, ImageFileExtension fileExtension) {
-        PutObjectPresignRequest request =
-                PutObjectPresignRequest.builder()
-                        .signatureDuration(Duration.ofMinutes(30))
-                        .putObjectRequest(
-                                builder ->
-                                        builder.bucket(s3Properties.bucket())
-                                                .key(fileName)
-                                                .contentType(getContentType(fileExtension))
-                                                .acl(ObjectCannedACL.PUBLIC_READ))
+    public String createUploadUrl(String fileName, ImageFileExtension fileExtension) {
+        String contentType = getContentType(fileExtension);
+
+        PutObjectRequest putObj =
+                PutObjectRequest.builder()
+                        .bucket(s3Properties.bucket())
+                        .key(fileName)
+                        .contentType(contentType) // content-type만 포함
                         .build();
-        return s3Presigner.presignPutObject(request).url().toString();
+
+        PutObjectPresignRequest presignReq =
+                PutObjectPresignRequest.builder()
+                        .putObjectRequest(putObj)
+                        .signatureDuration(Duration.ofMinutes(15))
+                        .build();
+
+        URL url = s3Presigner.presignPutObject(presignReq).url();
+        return url.toString();
     }
 
     private static String getContentType(ImageFileExtension fileExtension) {
