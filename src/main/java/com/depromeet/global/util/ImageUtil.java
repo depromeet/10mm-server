52b064022d6b7ebfce30f98365d6ede1912c29db
ybchar <uiurihappy@naver.com>
2025-09-26 20:55:52 +0900

    fix: remove presignedUrl ACL

--- src/main/java/com/depromeet/global/util/ImageUtil.java
+++ src/main/java/com/depromeet/global/util/ImageUtil.java
@@ -2,12 +2,14 @@ package com.depromeet.global.util;
 
 import com.depromeet.domain.image.domain.ImageFileExtension;
 import com.depromeet.infra.config.s3.S3Properties;
+import java.net.URL;
 import java.time.Duration;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import software.amazon.awssdk.services.s3.model.ObjectCannedACL;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
 
@@ -19,6 +21,7 @@ public class ImageUtil {
     private final S3Presigner s3Presigner;
     private final S3Properties s3Properties;
 
+    @Deprecated(since = "Don't use it by cloudflare ACL")
     @SneakyThrows
     public String createPreSignedUrl(String fileName, ImageFileExtension fileExtension) {
         PutObjectPresignRequest request =
@@ -32,6 +35,27 @@ public class ImageUtil {
                                                 .acl(ObjectCannedACL.PUBLIC_READ))
                         .build();
         return s3Presigner.presignPutObject(request).url().toString();
+    }
+
+    @SneakyThrows
+    public String createUploadUrl(String fileName, ImageFileExtension fileExtension) {
+        String contentType = getContentType(fileExtension);
+
+        PutObjectRequest putObj =
+                PutObjectRequest.builder()
+                        .bucket(s3Properties.bucket())
+                        .key(fileName)
+                        .contentType(contentType) // content-type만 포함
+                        .build();
+
+        PutObjectPresignRequest presignReq =
+                PutObjectPresignRequest.builder()
+                        .putObjectRequest(putObj)
+                        .signatureDuration(Duration.ofMinutes(15))
+                        .build();
+
+        URL url = s3Presigner.presignPutObject(presignReq).url();
+        return url.toString();
     }
 
     private static String getContentType(ImageFileExtension fileExtension) {
